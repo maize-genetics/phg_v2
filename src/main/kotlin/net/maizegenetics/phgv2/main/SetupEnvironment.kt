d84423ba39c41ffa28c338a36d8c69166bce9e38
lynnjo <lcj34@cornell.edu>
2023-10-03 13:48:43 -0400

    stub for SetupEnvironment

---
+++ src/main/kotlin/net/maizegenetics/phgv2/main/SetupEnvironment.kt
@@ -0,0 +1,18 @@
+package net.maizegenetics.phgv2.main
+
+import com.github.ajalt.clikt.core.CliktCommand
+
+/**
+ * Class to create the user conda environment for PHG.
+ * The environment will not be activated until a command is run
+ * that requires specific packages.
+ * The purpose of this script is to ensure the environment is created
+ * and available.
+ */
+class SetupEnvironment : CliktCommand() {
+
+        override fun run() {
+            // initial test
+            println("This is the SetupEnvironment script, which does nothing yet!")
+        }
+}
