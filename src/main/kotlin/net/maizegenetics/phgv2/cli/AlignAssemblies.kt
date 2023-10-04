32295f874a3ef9afa3abc99365ef2679b46c19d2
Terry Casstevens <tmc46@cornell.edu>
2023-10-04 17:11:37 -0400

    Initial implementation of AlignAssemblies command

--- src/main/kotlin/net/maizegenetics/phgv2/cli/AlignAssemblies.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/cli/AlignAssemblies.kt
@@ -5,7 +5,17 @@ import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.int
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.util.stream.Collectors
 
+/**
+ * This will align assemblies to a reference genome.
+ */
 class AlignAssemblies : CliktCommand() {
 
     val gff by option(help = "Full path to the reference gff file")
@@ -45,8 +55,242 @@ class AlignAssemblies : CliktCommand() {
         .int()
         .default(1)
 
+    data class InputChannelData(
+        val refFasta: String,
+        val asmFasta: String,
+        val outputDir: String,
+        val gffFile: String,
+        val refSamOutFile: String
+    )
+
     override fun run() {
-        // TBD
+
+        // create CDS fasta from reference and gff3 file
+        val cdsFasta = "$outputDir/ref.cds.fasta"
+
+        createCDSfromRefData(ref, gff, cdsFasta, outputDir)
+
+        // create list of assemblies to align from the assemblies file
+        val assembliesList = File(assemblies).readLines()
+
+        // run minimap2 for ref to refcds
+        val justNameRef = File(ref).nameWithoutExtension
+        val samOutFile = "${justNameRef}.sam"
+        val refSamOutFile = "${outputDir}/${samOutFile}"
+
+        runAnchorWaveMultiThread(ref, assembliesList, cdsFasta, gff, refSamOutFile)
+
+    }
+
+    private fun createCDSfromRefData(refFasta: String, gffFile: String, cdsFasta: String, outputDir: String): Boolean {
+
+        // val command = "anchorwave gff2seq -r ${refFasta} -i ${gffFile} -o ${cdsFasta} "
+        // Need to set the conda environment here to access anchorwave
+        val builder = ProcessBuilder(
+            "conda",
+            "run",
+            "-n",
+            "tiledb_anchorwave",
+            "anchorwave",
+            "gff2seq",
+            "-r",
+            refFasta,
+            "-i",
+            gffFile,
+            "-o",
+            cdsFasta
+        )
+
+        var redirectOutput = "$outputDir/anchorwave_gff2seq_output.log"
+        var redirectError = "$outputDir/anchorwave_gff2seq_error.log"
+        builder.redirectOutput(File(redirectOutput))
+        builder.redirectError(File(redirectError))
+
+        println("begin Command:" + builder.command().stream().collect(Collectors.joining(" ")))
+        try {
+            var process = builder.start()
+            var error = process.waitFor()
+            if (error != 0) {
+                println("createCDSfromRefData run via ProcessBuilder returned error code $error")
+                return false
+            }
+            return true
+        } catch (e: Exception) {
+            println("Error: could not execute anchorwave command. Run anchorwave manually and retry.")
+            return false
+        }
+
+    }
+
+    private fun runAnchorWaveMultiThread(
+        refFasta: String,
+        assemblies: List<String>,
+        cdsFasta: String,
+        gffFile: String,
+        refSamOutFile: String
+    ) {
+        runBlocking {
+            // Setup
+            val inputChannel = Channel<InputChannelData>()
+
+            // The input channel gets data needed to run minimap2 and align with anchorwave
+            launch {
+                println("Adding entries to the inputChannel:")
+                assemblies.forEach { asmFile ->
+
+                    // Column names were checked for validity above
+
+                    println("Adding: ${asmFile} for processing")
+                    //InputChannelData( val refFasta: String, val asmFasta: String, val asmDir: String, val asmDBName: String, val outputDir: String)
+                    inputChannel.send(InputChannelData(refFasta, asmFile, outputDir, gffFile, refSamOutFile))
+                }
+                println("Done Adding data to the inputChannel:")
+                inputChannel.close() //Need to close this here to show the workers that it is done adding more data
+            }
+
+            // This calls mummer4 scripts to process the alignments
+            val workerThreads = (1..runs).map { run ->
+                launch { alignAssembly(inputChannel, cdsFasta, gffFile) }
+            }
+
+            // Do not need a coroutine that "joins" the threads as they will all
+            // terminate above when there is no more data on the input channel
+        }
+    }
+
+    private suspend fun alignAssembly(inputChannel: Channel<InputChannelData>, cdsFasta: String, gffFile: String) =
+        withContext(
+            Dispatchers.Default
+        ) {
+            for (assemblyEntry in inputChannel) {
+                // Column names were checked for validity above
+                val justName = File(assemblyEntry.asmFasta).nameWithoutExtension
+                val samShort = "${justName}.sam"
+                val asmSamFile = "${assemblyEntry.outputDir}/${samShort}"
+
+                println("alignAssembly: asmFileFull: ${assemblyEntry.asmFasta}, outputFile: ${asmSamFile}")
+                //val threadnum = Integer.toString(Math.max(TasselPrefs.getMaxThreads() - 1, 1))
+                val builder = ProcessBuilder(
+                    "conda",
+                    "run",
+                    "-n",
+                    "tiledb_anchorwave",
+                    "minimap2",
+                    "-x",
+                    "splice",
+                    "-t",
+                    threads.toString(),
+                    "-k",
+                    "12",
+                    "-a",
+                    "-p",
+                    "0.4",
+                    "-N20",
+                    assemblyEntry.asmFasta,
+                    cdsFasta
+                )
+
+                var redirectError = "${assemblyEntry.outputDir}/minimap2_${justName}_error.log"
+                println("redirectError: ${redirectError}")
+                builder.redirectOutput(File(asmSamFile))
+                builder.redirectError(File(redirectError))
+                println(
+                    " begin minimap assembly Command:" + builder.command().stream().collect(Collectors.joining(" "))
+                );
+                var process = builder.start()
+                var error = process.waitFor()
+                if (error != 0) {
+                    println("minimap2 for assembly ${assemblyEntry.asmFasta} run via ProcessBuilder returned error code $error")
+                    throw IllegalStateException("alignAssembly: error running minimap2 for ${justName}: $error")
+                }
+                // We have the SAM File, call proali to align with anchorwave
+                runAnchorwaveProali(
+                    gffFile,
+                    assemblyEntry.refFasta,
+                    assemblyEntry.asmFasta,
+                    cdsFasta,
+                    assemblyEntry.refSamOutFile,
+                    asmSamFile
+                )
+            }
+        }
+
+    private fun runAnchorwaveProali(
+        gffFile: String,
+        refFasta: String,
+        asmFasta: String,
+        cdsFasta: String,
+        refSam: String,
+        asmSam: String
+    ) {
+
+        val justNameAsm = File(asmFasta).nameWithoutExtension
+        val justNameRef = File(refFasta).nameWithoutExtension
+
+        val anchorsproFile = "${outputDir}/${justNameAsm}_${justNameRef}.anchorspro"
+        //val outputFile ="${outputDir()}/${justNameAsm}_${justNameRef}.maf"
+        // Using just the assembly name.  This facilitates programmatically creating
+        // a GVCF keyfile from the maf keyfiles.  It will be understood that the maf
+        // file name is <assemblyFastaNoExtension>.maf
+        val outputFile = "${outputDir}/${justNameAsm}.maf"
+        val builder = ProcessBuilder(
+            "conda",
+            "run",
+            "-n",
+            "tiledb_anchorwave",
+            "anchorwave",
+            "proali",
+            "-i",
+            gffFile,
+            "-r",
+            refFasta,
+            "-as",
+            cdsFasta,
+            "-a",
+            asmSam,
+            "-ar",
+            refSam,
+            "-s",
+            asmFasta,
+            "-n",
+            anchorsproFile,
+            "-R",
+            refMaxAlignCov.toString(),
+            "-Q",
+            queryMaxAlignCov.toString(),
+            "-t",
+            threads.toString(),
+            "-o",
+            outputFile
+        )
+
+        var redirectError = "${outputDir}/proali_${justNameAsm}_outputAndError.log"
+        println("redirectError: ${redirectError}")
+        // NOTE: anchowave proali has an output file parameter, unlike minimap2, which uses
+        // command line redirection (ie >) to write the .SAM file
+        //
+        // Do not use the anchorwave output file parameter as the message redirect output
+        // for ProcessBuilder  or you'll end up with the text "AnchorWave done!" in the middle of
+        // a reference sequence, causing problems in parsing, and causing the sequence itself to be
+        // on a separate line from the data describing it.  Instead, send all program output messages
+        // to the error file.
+        // Here, we  write all program message output (ie non MAF file output) to a single file
+        // named as above.
+        builder.redirectOutput(File(redirectError))
+        builder.redirectError(File(redirectError))
+        println(
+            "begin proali Command for ${justNameAsm}:" + builder.command().stream().collect(Collectors.joining(" "))
+        );
+        try {
+            var process = builder.start()
+            var error = process.waitFor()
+            if (error != 0) {
+                println("proali for assembly ${asmFasta} run via ProcessBuilder returned error code $error")
+                throw IllegalStateException("runAnchorwaveProali: error running proali for ${justNameAsm}")
+            }
+        } catch (e: Exception) {
+            println("Error: could not execute anchorwave command. Run anchorwave manually and retry.")
+        }
     }
 
 }
