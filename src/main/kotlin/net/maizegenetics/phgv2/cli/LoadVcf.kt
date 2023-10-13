9856ee401f047fa78c58f995dd9a63a4f7a2bf01
lynnjo <lcj34@cornell.edu>
2023-10-13 08:20:24 -0400

    junit LoadVCF and code updates

--- src/main/kotlin/net/maizegenetics/phgv2/cli/LoadVcf.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/cli/LoadVcf.kt
@@ -48,6 +48,12 @@ class LoadVcf : CliktCommand() {
             }
         }
 
+    // threads is not required.  Defaults to 1
+    // While technically, this should be an "int", it is passed to ProcessBuilder as a string
+    // so we're leaving it defined as a string here.
+    val threads by option(help = "Number of threads for use by tiledb")
+        .default("1")
+
     override fun run() {
 
         // Check the type of files in the vcfDir
@@ -57,16 +63,29 @@ class LoadVcf : CliktCommand() {
 
         if (fileLists.first.isEmpty() && fileLists.second.isEmpty()) {
             // no gvcf or hvcf files found in the user supplied vcfDir
-            myLogger.warn("No gvcf or hvcf files found in $vcfDir.  Please check the folder and try again.")
+            myLogger.warn("No files ending in g.vcf.gz or h.vcf.gz found in $vcfDir.  Note that both the bgzipped and indexed files must exist in the specified folder. \nPlease check the folder and try again.")
             return
         }
 
-        // verify the URI is correct, create the datasets if they don't exist
-        // verifyURI() will create both datasets if they don't exist
-        if (fileLists.first.isNotEmpty()) {
 
+        // verify the URI is correct,
+        if (fileLists.first.isNotEmpty()) {
+            val gvcfExists = verifyURI(dbPath,"gvcf_dataset")
+            if (gvcfExists){
+                // Load the gvcf files!
+                val datasetURI = "${dbPath}/gvcf_dataset"
+                loadVCFToTiledb(fileLists.first, datasetURI, threads, tempDir)
+            }
         }
-       // val goodDbPath = verifyURI(dbPath) - add uri, determined by the list you have
+        if (fileLists.second.isNotEmpty()) {
+            val hvcfExists = verifyURI(dbPath,"hvcf_dataset")
+            if (hvcfExists){
+                // Load the hvcf files!
+                val datasetURI = "${dbPath}/hvcf_dataset"
+                loadVCFToTiledb(fileLists.first, datasetURI, threads, tempDir)
+            }
+        }
+
     }
 
     // This function walks the files in the vcf folder and returns a pair of lists,
@@ -79,9 +98,9 @@ class LoadVcf : CliktCommand() {
         val hvcfFiles = mutableListOf<String>()
         for (file in vcfFiles) {
             if (file.name.endsWith("g.vcf.gz") || file.name.endsWith("gvcf.gz")) {
-                gvcfFiles.add(file.name)
+                gvcfFiles.add(file.toString())
             } else if (file.name.endsWith("h.vcf.gz") || file.name.endsWith("hvcf.gz")) {
-                hvcfFiles.add(file.name)
+                hvcfFiles.add(file.toString())
             }
         }
         return Pair(gvcfFiles,hvcfFiles)
@@ -91,23 +110,22 @@ class LoadVcf : CliktCommand() {
     // The user determines the parent folder name where these datasets live
     // The actual tiledb dataset names are constant and are either gvcf_dataset or hvcf_dataset
 
-    // Are we just verifying the uri here, or creating it as well?  I think it would be ok to
-    // create them here.
+    // We are only verifying, not creating the datasets
     fun verifyURI(dbPath:String,uri:String): Boolean {
         // Check that the user supplied db folder exists
         check(File(dbPath).exists()) { "Folder $dbPath does not exist - please send a valid path that indicates the parent folder for your tiledb datasets." }
 
         // Check if the dataset exists
-        val dbfolder = File(dbPath)
-        val dataset = "${dbfolder.name}/${uri}"
-
+        val dataset = "${dbPath}/${uri}"
         val datasetPath = Paths.get(dataset)
-        if (Files.isRegularFile(datasetPath)) {
-            throw Exception("URI ${dataset}is a file, not a tiledb dataset folder.  The parent folder must not contain any files/folders named gvcf_dataset or hvcf_dataset that is not a tiledb created URI")
+
+        if (File(dataset).exists() && Files.isRegularFile(datasetPath)) {
+            throw IllegalArgumentException("URI ${dataset}is a file, not a tiledb dataset folder.  The parent folder must not contain any files/folders named gvcf_dataset or hvcf_dataset that is not a tiledb created URI")
         }
 
         if (File(dataset).exists()  && Files.isDirectory(Paths.get(dataset))){
-            var builder = ProcessBuilder("conda","run","-n","phgv2-conda","tiledbvcf","stat","--uri",uri)
+            // check if is a tiledb dataset
+            var builder = ProcessBuilder("conda","run","-n","phgv2-conda","tiledbvcf","stat","--uri",dataset)
             var redirectOutput = tempDir + "/tiledb_statURI_output.log"
             var redirectError = tempDir + "/tiledb_statURI_error.log"
             builder.redirectOutput( File(redirectOutput))
@@ -129,7 +147,6 @@ class LoadVcf : CliktCommand() {
                 println("Error: could not run tiledb stat on ${uri}.")
                 throw IllegalArgumentException("Error running ProcessBuilder to stat tiledb URI: ${exc.message}")
             }
-            // check if is a tiledb dataset
 
             myLogger.info("Using existing TileDB datasets previously created in folder $dbPath.")
             return true
@@ -137,7 +154,36 @@ class LoadVcf : CliktCommand() {
             myLogger.info("TileDB datasets not found in folder $dbPath. Please run InitDB to create the datasets.")
             return false
         }
+    }
 
+    fun loadVCFToTiledb(vcfList:List<String>, uri:String, threads:String, tempDir:String) {
+        // get just last part of uri string, ie just the last name in this folder
+        val uriName = uri.split("/").last()
+        val vcfListFile = "${tempDir}/${uriName}_vcfList.txt"
+        println("LoadTiledbH: gvcfList: ${vcfList}")
+        File(vcfListFile).writeText(vcfList.joinToString("\n"))
+
+        // Store the files to tiledb
+        var builder = ProcessBuilder("conda","run","-n","phgv2-conda","tiledbvcf","store","--uri",uri,"-t",threads,"-f",vcfListFile,"--remove-sample-file")
+        var redirectOutput = tempDir + "/tiledb_store_output.log"
+        var redirectError = tempDir + "/tiledb_store_error.log"
+        builder.redirectOutput( File(redirectOutput))
+        builder.redirectError( File(redirectError))
+
+        println("begin Command:" + builder.command().stream().collect(Collectors.joining(" ")))
+        try {
+            var process = builder.start()
+            var error = process.waitFor()
+            if (error != 0) {
+                println("loadVCFToTiledb run via ProcessBuilder returned error code ${error}. See ${redirectError} and ${redirectOutput} for more details.")
+                throw IllegalArgumentException("Error running ProcessBuilder to store vcfs to tiledb ${uri} array: ${error}")
+            }
+
+        } catch (exc: Exception) {
+            println("Error: loadVCFToTiledb: could not load tiledb array ${uri}.  See ${redirectError} and ${redirectOutput} for more details.")
+            throw IllegalArgumentException("Error running ProcessBuilder to store vcfs to tiledb array: ${exc.message}")
+        }
+        println("Finished! vcfs stored to tiledb dataset ${uri}")
     }
 
 }
--- src/test/kotlin/net/maizegenetics/phgv2/cli/LoadVCFTest.kt
+++ src/test/kotlin/net/maizegenetics/phgv2/cli/LoadVCFTest.kt
@@ -1,20 +1,41 @@
 package net.maizegenetics.phgv2.cli
 
 import com.github.ajalt.clikt.testing.test
+import com.google.common.io.Files
+import net.maizegenetics.phgv2.utils.VariantLoadingUtilsTest
+import net.maizegenetics.phgv2.utils.bgzipAndIndexGVCFfile
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
+import java.io.File
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 @ExtendWith(TestExtension::class)
 class LoadVCFTest {
     companion object {
+        val tempDir = "${System.getProperty("user.home")}/temp/phgv2Tests/tempDir/"
 
+        @JvmStatic
+        @BeforeAll
+        fun setup() {
+            File(tempDir).mkdirs()
+        }
+
+        // Comment out the tearDown()if you need to look at the logs files created by ProcessBuilder
+        // commands.
+        @JvmStatic
+        @AfterAll
+        fun teardown() {
+            File(tempDir).deleteRecursively()
+        }
     }
 
     @Test
     fun testCliktParams() {
         val loadVCF = LoadVcf()
-        val resultGood = loadVCF.test("--vcf-dir ${TestExtension.testVCFDir} --db-path ${TestExtension.testTileDBURI}")
 
         // Test missing vcf-dir parameter
         val resultMissingVCFDir = loadVCF.test("--db-path ${TestExtension.testTileDBURI} --temp-dir ${TestExtension.tempDir}")
@@ -37,4 +58,125 @@ class LoadVCFTest {
                 "\n" +
                 "Error: invalid value for --temp-dir: --temp-dir must not be blank\n",resultMissingTempDir.output)
     }
+
+    @Test
+    fun testBadURIs() {
+
+        val badPath = "${TestExtension.tempDir}badPATH"
+        val URI = "${TestExtension.tempDir}hvcf_dataset"
+        //Test non-existant dbPath
+        assertThrows<IllegalStateException> {
+            //Check that an error is thrown when the dbPath folder does not exist
+            LoadVcf().verifyURI(badPath, URI)
+        }
+
+        // Test tiledb URI is a file, not a tiledb array
+        val goodPath = "${TestExtension.tempDir}"
+        val fileURI = "hvcf_dataset"
+        val filePath = "${TestExtension.tempDir}${fileURI}"
+
+        File(filePath).bufferedWriter().use {
+            it.write("this is just a text file")
+        }
+        assertThrows<IllegalArgumentException> {
+            //Check that an error is thrown when the dbPath is good, but the URI is a regular file, not a tiledb array
+            LoadVcf().verifyURI(goodPath, fileURI)
+        }
+
+        // Test tiledbURI is a directory, but not a tiledb array
+        // delete the filePath created above
+        File(filePath).delete()
+
+        // Recreate it as a directory
+        val dirURI = "${TestExtension.tempDir}hvcf_dataset"
+        File(dirURI).mkdirs()
+        assertThrows<IllegalStateException> {
+            //Check that an error is thrown when the dbPath is good, but the URI is a regular directory file, not a tiledb array
+            LoadVcf().verifyURI(goodPath, fileURI)
+        }
+    }
+
+    @Test
+    fun testGetFileLists() {
+        // Change to TestExtension when that is updated to reference the files in data/test/smallseq
+
+        // copy the files from data/test/smallseq to the tempDir
+        // call bgzip to get the files we need for the test
+        var origGvcfFile = "data/test/smallseq/LineA.g.vcf"
+        var testGvcfFile = "${TestExtension.testVCFDir}LineA.g.vcf"
+        Files.copy(File(origGvcfFile), File(testGvcfFile))
+
+        val origHvcfFile = "data/test/smallseq/Ref.h.vcf"
+        val testHvcfFile = "${TestExtension.testVCFDir}Ref.h.vcf"
+        Files.copy(File(origHvcfFile), File(testHvcfFile))
+
+        // call bgzipAndIndexGVCFfile to zip and index the file
+        bgzipAndIndexGVCFfile(testGvcfFile)
+        bgzipAndIndexGVCFfile(testHvcfFile)
+
+        // Folder with both g.vcf.gz and h.vcf.gz files, and other files
+        // verify the g.vcf.gz is returned in lists.first, and h.vcf.gz is returned in lists.second
+        var vcfDir = TestExtension.testVCFDir
+        val lists = LoadVcf().getFileLists(vcfDir)
+        assertEquals(lists.first.size, 1)
+        assertEquals(lists.second.size, 1)
+        assertEquals(lists.first[0], "${TestExtension.testVCFDir}LineA.g.vcf.gz")
+        assertEquals(lists.second[0], "${TestExtension.testVCFDir}Ref.h.vcf.gz")
+
+        // Check a folder with no g.vcf.gz or h.vcf.gz files
+        // THe docs folder has no vcf files, so the lists should be empty
+        vcfDir = "docs"
+        val emptyLists = LoadVcf().getFileLists(vcfDir)
+        assertEquals(emptyLists.first.size, 0)
+        assertEquals(emptyLists.second.size, 0)
+
+    }
+
+    @Test
+    fun testLoadVCF() {
+        // first create the tiledb arrays by calling Initdb
+        val dbPath = "${TestExtension.testTileDBURI}"
+        // make the dbPath directory if it does not exist
+        File(dbPath).mkdirs()
+        Initdb().createDataSets(dbPath)
+
+        // verify the dbPath directory exists with subdirectories hvcf_dataset and gvcf_dataset
+        val dbDir = File(dbPath)
+        assertEquals(dbDir.exists(), true)
+        assertEquals(dbDir.isDirectory, true)
+        assertEquals(dbDir.listFiles().size, 3) // includes a temp dir created for log file output
+        assertTrue(dbDir.listFiles().map { it.name }.contains("hvcf_dataset"))
+        assertTrue(dbDir.listFiles().map { it.name }.contains("gvcf_dataset"))
+
+        //assertEquals(dbDir.listFiles()[0].name, "hvcf_dataset")
+        //assertEquals(dbDir.listFiles()[1].name, "gvcf_dataset")
+
+        // copy the files from data/test/smallseq to the tempDir
+        // call bgzip to get the files we need for the test
+        var origGvcfFile = "data/test/smallseq/LineA.g.vcf"
+        var testGvcfFile = "${TestExtension.testVCFDir}LineA.g.vcf"
+        Files.copy(File(origGvcfFile), File(testGvcfFile))
+
+        val origHvcfFile = "data/test/smallseq/Ref.h.vcf"
+        val testHvcfFile = "${TestExtension.testVCFDir}Ref.h.vcf"
+        Files.copy(File(origHvcfFile), File(testHvcfFile))
+
+        // call bgzipAndIndexGVCFfile to zip and index the file
+        bgzipAndIndexGVCFfile(testGvcfFile)
+        bgzipAndIndexGVCFfile(testHvcfFile)
+
+
+        // now load the vcf files stored in the data/test/smallseq folder
+        val loadVCF = LoadVcf()
+        val vcfDir = TestExtension.testVCFDir
+        val result = loadVCF.test("--vcf-dir ${vcfDir} --db-path ${dbPath} --temp-dir ${TestExtension.tempDir}")
+
+        // to verify the load worked, need to query tiledb for sample names
+        // first get the sample names from the vcf files
+        // THis isn't going to work until we have means to query tiledb arrays.
+
+
+    }
+
+
 }
