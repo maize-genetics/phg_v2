7c673a75d80238859c3ed04f919aafeca8544b27
Terry Casstevens <tmc46@cornell.edu>
2023-10-13 12:21:47 -0400

    Added tests for existance of .maf files to AlignAssembliesTest

--- src/test/kotlin/net/maizegenetics/phgv2/cli/AlignAssembliesTest.kt
+++ src/test/kotlin/net/maizegenetics/phgv2/cli/AlignAssembliesTest.kt
@@ -1,9 +1,12 @@
 package net.maizegenetics.phgv2.cli
 
+import biokotlin.genome.getMAFblocks
 import com.github.ajalt.clikt.testing.test
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import java.io.File
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 @ExtendWith(TestExtension::class)
 class AlignAssembliesTest {
@@ -24,6 +27,20 @@ class AlignAssembliesTest {
         )
 
         assertEquals(result.statusCode, 0, "status code not 0: ${result.statusCode}")
+
+        assertTrue(
+            File(TestExtension.smallseqLineAMafFile).exists(),
+            "File ${TestExtension.smallseqLineAMafFile} does not exist"
+        )
+
+        assertTrue(
+            File(TestExtension.smallseqLineBMafFile).exists(),
+            "File ${TestExtension.smallseqLineBMafFile} does not exist"
+        )
+
+        // val mafBlocks = getMAFblocks("${TestExtension.tempDir}/LineA.maf")
+
+        // mafBlocks[0].forEach { println(it) }
 
     }
 
