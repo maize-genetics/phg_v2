b0f6bca5f55cb10889b654bede263814c6148cec
lynnjo <lcj34@cornell.edu>
2023-10-23 14:09:10 -0400

    moving agc retrieve commands to utils/SeqUtils file

---
+++ src/main/kotlin/net/maizegenetics/phgv2/utils/SeqUtils.kt
@@ -0,0 +1,99 @@
+package net.maizegenetics.phgv2.utils
+
+import biokotlin.seq.NucSeq
+import java.io.BufferedInputStream
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.util.logging.Logger
+
+private val myLogger = Logger.getLogger("net.maizegenetics.phgv2.utils.SeqUtils")
+
+// Retrieve AGC data from user command list.  The command list should
+// have components that look like one of the following:
+//  contig@genome:start-end
+//  contig@genome
+//  contig@genome:start-end
+//
+fun retrieveAgcData(dbPath:String,ranges:List<String>):Map<String, NucSeq> {
+
+    val commands = buildAgcCommandFromList(dbPath, ranges)
+    val chromNucSeqMap = queryAgc(commands)
+    return chromNucSeqMap
+}
+
+// THis builds the command that is sent to AGC.  It creates the initial
+// command, which sets the conda environment, then adds the AGC file
+// path, then follows with the commands the user has put together in a list
+fun buildAgcCommandFromList(dbPath:String, ranges:List<String>): Array<String> {
+    // this is meant to be called from other programs.  it will return an array
+    // of commands that can be sent to agc
+
+    // Validate the dbPath. We check if the folder exists, and if it contains the file assemblies.agc
+    val agcFile = "${dbPath}/assemblies.agc"
+    if (File(agcFile).exists()) println("Found AGC file: ${agcFile}")
+    else println("Did not find AGC file: ${agcFile}")
+
+    check(File(agcFile).exists()) { "File assemblies.agc does not exist in folder $dbPath- please send a valid path that indicates the parent folder for your assemblies.agc compressed file." }
+
+    val command = mutableListOf<String>()
+    val commandPrefix = arrayOf("conda","run","-n","phgv2-conda","agc","getctg",agcFile)
+    command.addAll(commandPrefix)
+    command.addAll(ranges)
+
+    return command.toTypedArray()
+}
+
+// Retrieve AGC data from a list of commands
+fun queryAgc(commands:Array<String>):Map<String, NucSeq> {
+    check(commands.size > 0) { "Error:  No commands sent to retrieveAgcData!" }
+    myLogger.info("Running Agc Command:\n${commands.joinToString(" ")}")
+    println("Running Agc Command:\n${commands.joinToString(" ")}")
+    val agcProcess = ProcessBuilder(*commands)
+        .redirectError(ProcessBuilder.Redirect.INHERIT)
+        .start()
+
+
+    val agcOut = BufferedInputStream(agcProcess.inputStream, 5000000)
+
+    // Note AGC does not include the genome in the idline, so we
+    // can end up with the same idline multiple times, e.g. if we want chrom 1 from
+    // multiple genomes, all the idlines will be ">1" and we will only get the last one
+    // when processing into a map.
+    // It is the same if we give a region for the chrom, e.g. 1@LineA:1-1000 and 1@LineB:1-1000,
+    // we will get the same idline multiple times.
+    val chromNucSeqMap = HashMap<String, NucSeq>()
+    try {
+        agcOut.bufferedReader().use { br ->
+            var currChrom: String = "-1"
+            var currSeq = ByteArrayOutputStream()
+            var line = br.readLine()
+            while (line != null) {
+
+                line = line.trim()
+                if (line.startsWith(">")) {
+                    if (currChrom != "-1") {
+                        // finished with this chromosome's sequence
+                        println("fastaToNucSeq: finished chrom $currChrom")
+                        chromNucSeqMap.put(currChrom, NucSeq(currSeq.toString()))
+                    }
+                    // reset chromosome name and sequence, begin processing next chrom
+                    currChrom = line.substring(1).split(" ")[0]
+                    currSeq = ByteArrayOutputStream()
+                } else {
+                    currSeq.write(line.toByteArray())
+                }
+                line = br.readLine()
+            }
+            if (currSeq.size() > 0) {
+                println("fastaToNucSeq: finished chrom $currChrom")
+                chromNucSeqMap.put(currChrom, NucSeq(currSeq.toString()))
+            }
+        }
+    } catch (exc:Exception) {
+        myLogger.severe("Error:  Exception in retrieveAgcData: ${exc.message}")
+        throw IllegalStateException("Error:  Exception in retrieveAgcData: ${exc.message}")
+    }
+
+    // return agcOut
+    return chromNucSeqMap
+}
---
+++ src/test/kotlin/net/maizegenetics/phgv2/utils/SeqUtilsTest.kt
@@ -0,0 +1,174 @@
+package net.maizegenetics.phgv2.utils
+
+import com.github.ajalt.clikt.testing.test
+import net.maizegenetics.phgv2.cli.AgcCompress
+import net.maizegenetics.phgv2.cli.CreateRefVcf
+import net.maizegenetics.phgv2.cli.TestExtension
+import net.maizegenetics.phgv2.cli.TestExtension.Companion.testOutputFastaDir
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+
+class SeqUtilsTest {
+    companion object {
+
+        val tempDir = "${System.getProperty("user.home")}/temp/phgv2Tests/tempDir/"
+        @JvmStatic
+        @BeforeAll
+        fun setup() {
+            File(TestExtension.tempDir).mkdirs()
+
+            // create the agc compressed file from which we'll pull data
+            val fastaInputDir = "data/test/smallseq"
+            val fastaOutputDir = TestExtension.testOutputFastaDir
+            val dbPath = TestExtension.testTileDBURI
+
+            // Why isn't this handled in TestExtension - I have added the command "File(testTileDBURI).mkdirs()"
+            // to TestExtension
+            Files.createDirectories(Paths.get(dbPath))
+
+            // copy files with extension .fa from data/test/smallseq to fastaOutputDir
+            val fastaFiles = File(fastaInputDir).listFiles { file -> file.extension == "fa" }
+            fastaFiles.forEach { file -> file.copyTo(File(fastaOutputDir, file.name)) }
+
+
+            // get the full path fasta file names  from the fastaInput, write to fileList
+            val fileList = mutableListOf<String>()
+            File(fastaOutputDir).walk(FileWalkDirection.TOP_DOWN).filter{it.name.endsWith(".fa")}.forEach {
+                fileList.add(it.toString())
+            }
+
+            // The list of fasta files to load should not contain the reference.  The reference is listed separately.
+            fileList.removeIf { it.contains("Ref") }
+
+            // write the full path fasta file names  from the fastaOutputDir to a single file, one per line, in tempDir
+            // This file will be used as input to the agc-compress command
+            val fastaCreateFileNamesFile = File(dbPath, "fastaCreateFileNames.txt")
+            fastaCreateFileNamesFile.writeText(fileList.joinToString("\n"))
+
+            val refFasta = File(fastaOutputDir, "Ref.fa").toString()
+
+            val agcCompress = AgcCompress()
+            // Create the initial compressed file
+            println("Calling agcCompress for CREATE")
+            var result = agcCompress.test("--fasta-list ${fastaCreateFileNamesFile} --db-path ${dbPath} --ref-fasta ${refFasta}")
+
+        }
+
+//        @JvmStatic
+//        @AfterAll
+//        fun teardown() {
+//            File(TestExtension.tempDir).deleteRecursively()
+//        }
+    }
+
+    @Test
+    fun testBuildAgcCommandFromList() {
+        // test command with single range on the list
+
+        val dbPath = TestExtension.testTileDBURI
+        var rangeList = mutableListOf<String>()
+        val range1 = "1@LineA:20-40"
+        rangeList.add(range1)
+        var command = buildAgcCommandFromList(dbPath,rangeList)
+
+        //The command should look like:
+        val expectedCommand = arrayOf("conda","run","-n","phgv2-conda","agc","getctg",dbPath,range1)
+
+        // Verify that command contains all elements from expected command and that the order is the same
+        kotlin.test.assertEquals(expectedCommand.size, command.size)
+        for (i in 0..expectedCommand.size-1) {
+            kotlin.test.assertEquals(expectedCommand[i], command[i])
+        }
+
+        // Add multiple ranges to the list.  Verify the command contains all range requests
+        val range2 = "1@LineA:60-80"
+        val range3 = "1@LineA:100-120"
+
+        rangeList.add(range2)
+        rangeList.add(range3)
+        command = buildAgcCommandFromList(dbPath,rangeList)
+        val expectedCommand2 = arrayOf("conda","run","-n","phgv2-conda","agc","getctg",dbPath,range1,range2,range3)
+        kotlin.test.assertEquals(expectedCommand2.size, command.size)
+        for (i in 0..expectedCommand2.size-1) {
+            kotlin.test.assertEquals(expectedCommand2[i], command[i])
+        }
+
+    }
+
+    @Test
+    fun testRetrieveAgcData() {
+        // This tests the function retrieveAgcData.  retrieveAgcData() calls buildAgcCommandFromList() and
+        // and then queryAgc() to get the data from the agc compressed file.  The data is returned as a
+        // Map<String,NucSeq> where "String" is idline from the AGC created fasta, and NucSeq is the sequence.
+
+        val dbPath = "${TestExtension.testTileDBURI}" // just the path, code appends "assemblies.agc"
+        var rangeList = mutableListOf<String>()
+        val range1 = "1@LineA:0-19" // AGC queries are 0-based !!
+        rangeList.add(range1)
+        var agcResult = retrieveAgcData(dbPath,rangeList)
+
+        // this has only a single query, so the result should have 1 entry: 1 key and 1 sequence
+        assertEquals(1, agcResult.size)
+        assertEquals(1, agcResult.keys.size)
+        // verify the key as "1:0-19".  The genome is not included in AGC's id line
+        assertEquals("1:0-19", agcResult.keys.first())
+        // verify the sequence - this is copied from LineA:1-20 in the fasta file
+        assertEquals("GCGCGGGGACCGAGAAACCC", agcResult.values.first().toString())
+
+        // add additional ranges to the list
+        val range2 = "1@LineA:20-39"
+        val range3 = "1@LineA:40-59"
+
+        rangeList.add(range2)
+        rangeList.add(range3)
+        agcResult = retrieveAgcData(dbPath,rangeList)
+        // this has 3 queries, so the result should have 3 entries: 3 keys and 3 sequences
+        assertEquals(3, agcResult.size)
+        assertEquals(3, agcResult.keys.size)
+
+        // This is a map, the keys will not be in any specific order, so verify
+        // that each key exists in the list of expected keys
+        val keyList = mutableListOf<String>("1:0-19","1:20-39","1:40-59")
+        // verify the map returned contains all keys from the keyList
+        assertTrue(agcResult.keys.containsAll(keyList))
+
+        // verify the sequences: these are copied from LineA:1-20, LineA:21-40, and LineA:41-60 in the fasta file
+        // used in the setup() function
+        assertEquals("GCGCGGGGACCGAGAAACCC", agcResult["1:0-19"].toString())
+        assertEquals("GGCGGGGCAGGACGAACCGG", agcResult["1:20-39"].toString())
+        assertEquals("GCGAGAACGGACAACCCCCC", agcResult["1:40-59"].toString())
+
+    }
+
+    @Test
+    fun testBadAGCParameter() {
+        // This function tests what happens when a bad parameter is passed to the agc command
+        // The agc command should return an error message, and the function should throw an exception
+
+        // This db path does not exist
+        var dbPath = "/my/bad/dbPath"
+        var rangeList = mutableListOf<String>()
+        val range1 = "1@LineA:0-19" // AGC queries are 0-based !!
+        rangeList.add(range1)
+        assertThrows<IllegalStateException> {
+            //Check that an error is thrown when the dbPath is not a directory that contains the assemblies.agc file
+            retrieveAgcData(dbPath,rangeList)
+        }
+
+        // Test with a valid directory name, but the assemblies.agc file does not exist
+
+        dbPath = testOutputFastaDir
+        assertThrows<IllegalStateException> {
+            //Check that an error is thrown when the dbPath is not a directory that contains the assemblies.agc file
+            retrieveAgcData(dbPath,rangeList)
+        }
+
+    }
+}
