a62a5e924f6e609e4bc966be5e50075dc4a792f8
lynnjo <lcj34@cornell.edu>
2023-11-29 10:34:33 -0500

    fixed multithreading

--- src/main/kotlin/net/maizegenetics/phgv2/agc/AnnotateFasta.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/agc/AnnotateFasta.kt
@@ -14,14 +14,19 @@ import org.apache.logging.log4j.LogManager
 import java.io.File
 
 /**
- * This class takes a list of fasta files and updates the idline with the sample name.
+ * This class takes a list of fasta files and updates each idline with the sample name.
  * The sample name is derived from the fasta file name, minus the extension.
  * Fasta files should be named with just the sample name, extension can be either .fa or .fasta.
+ * These updates are required as AGC processing maintains  fasta comments, but it does not maintain the
+ * sample name when included with a query request.
  *
- * This is consistent naming with the AGG compress code..  AGC loads the fasta name minus extension as
- * the sample name.
+ * The naming conventions are consistent with the naming requirements of the AGG compress code.  AGC loads the fasta name
+ * minus extension as the sample name when creating their compressed file.
  *
- * This just a helper class.  Users may already have updated their fasta files.
+ * When determining the number of threads to use, keep in mind the memory capacity of the machine,
+ * and the size of the fasta files.  Each fasta is read into memory, idlines updated, then re-written
+ * to a new file of the same name in the user specified output directory.
+ *
  */
 class AnnotateFasta : CliktCommand() {
     private val myLogger = LogManager.getLogger(AnnotateFasta::class.java)
@@ -59,56 +64,37 @@ class AnnotateFasta : CliktCommand() {
         createParallelAnnotatedFastas(assembliesList, outputDir)
     }
 
-//    fun createAnnotatedFastas(fastaList:String, outputDir:String) {
-//        val fastaFiles = File(fastaList).readLines()
-//        fastaFiles.forEach { fastaFile ->
-//            // Write all lines in the fasta file to the new file.
-//            // the sample name is the filename minus the extension
-//            // the new file name is the same as the original but it is
-//            // written to the outputDir
-//            // if  the line starts with >, append " sampleName=${sampleName}" to the line
-//            val sampleName = File(fastaFile).nameWithoutExtension
-//            val newFilename = "${outputDir}/${File(fastaFile).name}"
-//            File(newFilename).bufferedWriter().use { writer ->
-//                File(fastaFile).forEachLine { line ->
-//                    if (line.startsWith(">")) {
-//                        // Only append if the line does not already contain "sampleName=${sampleName}"
-//                        if (!line.contains("sampleName=${sampleName}") )
-//                             writer.write("${line} sampleName=${sampleName}\n")
-//                        else
-//                            writer.write("${line}\n")
-//                    } else {
-//                        writer.write("${line}\n")
-//                    }
-//                }
-//            }
-//        }
-//    }
 
+    // This method sets up parallel processing of the fasta files.
+    // The files are placed in an input queue, worker threads are launched
+    // to process the queue entries.  The number of worker threads is based on the
+    // number of threads specified by the user.
     private fun createParallelAnnotatedFastas(assemblies:List<String>, outputDir:String) {
-        println("createParallelAnnotatedFastas: assemblies: ${assemblies.joinToString("\n")}")
+
         runBlocking {
             // Setup
-            val inputChannel = Channel<InputChannelData>()
+            val inputChannel = Channel<InputChannelData>(100)
             launch {
                 myLogger.info("Adding entries to the inputChannel:")
-                println("adding entries to the inputChannel , size of assemblies = ${assemblies.size} ")
                 assemblies.forEach { asmFile ->
                     val sampleName = File(asmFile).nameWithoutExtension
                     println("adding ${sampleName} to the inputChannel")
                     inputChannel.send(InputChannelData(asmFile, sampleName, outputDir))
                 }
-                println("Done adding entries to the inputChannel")
                 myLogger.info("Done adding data to the inputChannel")
                 inputChannel.close()
 
-                val workerThreads = (1..threads).map {
-                    launch { annotateFasta(inputChannel) }
-                }
+            }
+
+            val workerThreads = (1..threads).map {run ->
+                launch { annotateFasta(inputChannel) }
             }
         }
     }
 
+    // This function takes a list of fasta files and to each idline in each fasta file,
+    // appends " sampleName=<sampleName>" where sampleName is the fasta file name minus the extension
+    // If the idline already contains " sampleName=<sampleName>" then it is not appended.
     private suspend fun annotateFasta(inputChannel: Channel<InputChannelData>) =
         withContext (
             Dispatchers.Default
