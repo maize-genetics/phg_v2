be304442d5c4fcb87467b886fd5e80213b1f25d0
lynnjo <lcj34@cornell.edu>
2023-12-18 09:14:47 -0500

    updated build.gradle.kts to inclue serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.9.10"
     application
     id("org.jetbrains.kotlinx.kover") version "0.7.3"
+    kotlin("plugin.serialization") version "1.6.21"
 }
 
 group = "net.maizegenetics"
