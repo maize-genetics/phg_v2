485269711c18dc68808b8a63ed42a2cc05c6bd1e
lynnjo <lcj34@cornell.edu>
2023-12-18 15:18:49 -0500

    updated gradle for Http and conf file for TILEDB_URI

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,6 +67,9 @@ dependencies {
     implementation("io.ktor:ktor-server-default-headers:$ktorVersion")
     implementation("io.ktor:ktor-server-call-logging:$ktorVersion")
     implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.0")
 
