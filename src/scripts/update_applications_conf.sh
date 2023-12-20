b69add809542650ae7845c48b8f6fc43e10c7202
lynnjo <lcj34@cornell.edu>
2023-12-20 09:25:49 -0500

    file location fix in update_applicatons_conf.sh attempt 1

--- src/scripts/update_applications_conf.sh
+++ src/scripts/update_applications_conf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 TILEDB_URI="$HOME/temp/phgv2Tests/tempDir/testTileDBURI/"
 # This adds the TILEDB_URI line to the beginning of the application.conf file
-echo -e "TILEDB_URI=$TILEDB_URI\n$(cat ../main/resources/application.conf)" > ../main/resources/application.conf
+echo -e "TILEDB_URI=$TILEDB_URI\n$(cat src/main/resources/application.conf)" > src/main/resources/application.conf
