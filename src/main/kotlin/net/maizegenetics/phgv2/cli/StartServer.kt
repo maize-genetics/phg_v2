ff9ebea22203d7e375d122020a71611577f29d57
lynnjo <lcj34@cornell.edu>
2023-12-21 11:38:11 -0500

    moved verifyURI from LoadVCF to VariantLoadingUtils.kt for general use

--- src/main/kotlin/net/maizegenetics/phgv2/cli/StartServer.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/cli/StartServer.kt
@@ -1,21 +1,42 @@
 package net.maizegenetics.phgv2.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.validate
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import net.maizegenetics.phgv2.utils.setupDebugLogging
+import net.maizegenetics.phgv2.utils.verifyURI
 import org.apache.logging.log4j.LogManager
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class StartServer : CliktCommand(help = "Starts PHGv2 BrAPI Server") {
 
     private val myLogger = LogManager.getLogger(StartServer::class.java)
 
+    val dbPath by option(help = "Folder name where TileDB datasets are stored.   ")
+        .default("")
+        .validate {
+            require(it.isNotBlank()) {
+                "--db-path must not be blank"
+            }
+        }
+
     override fun run() {
         setupDebugLogging()
+
+        // Verify the uri is valid.  We only care about the hvcf dataset,
+        // so check that one explicitly
+        val hvcfExists = verifyURI(dbPath,"hvcf_dataset")
 
         // commandLineEnvironment reads the application.config file
         // https://ktor.io/docs/configuration.html#hocon-file
         embeddedServer(Netty, commandLineEnvironment(emptyArray())).start(wait = true)
     }
+
+
 
 }
--- src/test/kotlin/net/maizegenetics/phgv2/cli/LoadVCFTest.kt
+++ src/test/kotlin/net/maizegenetics/phgv2/cli/LoadVCFTest.kt
@@ -3,6 +3,7 @@ package net.maizegenetics.phgv2.cli
 import com.github.ajalt.clikt.testing.test
 import com.google.common.io.Files
 import net.maizegenetics.phgv2.utils.bgzipAndIndexGVCFfile
+import net.maizegenetics.phgv2.utils.verifyURI
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeAll
@@ -60,7 +61,7 @@ class LoadVCFTest {
         //Test non-existant dbPath
         assertThrows<IllegalStateException> {
             //Check that an error is thrown when the dbPath folder does not exist
-            LoadVcf().verifyURI(badPath, URI)
+            verifyURI(badPath, URI)
         }
 
         // Test tiledb URI is a file, not a tiledb array
@@ -73,7 +74,7 @@ class LoadVCFTest {
         }
         assertThrows<IllegalArgumentException> {
             //Check that an error is thrown when the dbPath is good, but the URI is a regular file, not a tiledb array
-            LoadVcf().verifyURI(goodPath, fileURI)
+            verifyURI(goodPath, fileURI)
         }
 
         // Test tiledbURI is a directory, but not a tiledb array
@@ -85,7 +86,7 @@ class LoadVCFTest {
         File(dirURI).mkdirs()
         assertThrows<IllegalArgumentException> {
             //Check that an error is thrown when the dbPath is good, but the URI is a regular directory file, not a tiledb array
-            LoadVcf().verifyURI(goodPath, fileURI)
+            verifyURI(goodPath, fileURI)
         }
     }
 
