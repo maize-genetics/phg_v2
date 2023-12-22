7f5b25a039cc803123d801c7c041f42e58505786
lynnjo <lcj34@cornell.edu>
2023-12-22 08:27:49 -0500

    added args to embeddedServer() call

--- src/main/kotlin/net/maizegenetics/phgv2/cli/StartServer.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/cli/StartServer.kt
@@ -9,15 +9,14 @@ import io.ktor.server.netty.*
 import net.maizegenetics.phgv2.utils.setupDebugLogging
 import net.maizegenetics.phgv2.utils.verifyURI
 import org.apache.logging.log4j.LogManager
-import java.io.File
-import java.nio.file.Files
 import java.nio.file.Paths
+import java.util.stream.Collectors
 
 class StartServer : CliktCommand(help = "Starts PHGv2 BrAPI Server") {
 
     private val myLogger = LogManager.getLogger(StartServer::class.java)
 
-    val dbPath by option(help = "Folder name where TileDB datasets are stored.   ")
+    val dbPath by option(help = "Full path to folder where TileDB datasets are stored.   ")
         .default("")
         .validate {
             require(it.isNotBlank()) {
@@ -31,10 +30,18 @@ class StartServer : CliktCommand(help = "Starts PHGv2 BrAPI Server") {
         // Verify the uri is valid.  We only care about the hvcf dataset,
         // so check that one explicitly
         val hvcfExists = verifyURI(dbPath,"hvcf_dataset")
+        if (!hvcfExists) {
+            myLogger.error("hvcf_dataset does not exist in $dbPath.  Exiting.")
+            return
+        }
+
+        // Create and Args list to pass to the server
+        // This tells the endpoint code where the datasets are located.
+        val args = listOf("TILEDB_URI=${dbPath}").toTypedArray()
 
         // commandLineEnvironment reads the application.config file
         // https://ktor.io/docs/configuration.html#hocon-file
-        embeddedServer(Netty, commandLineEnvironment(emptyArray())).start(wait = true)
+        embeddedServer(Netty, commandLineEnvironment(args)).start(wait = true)
     }
 
 
