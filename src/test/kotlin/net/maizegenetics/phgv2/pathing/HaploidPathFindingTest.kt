debf54f84b3698e24d81cc0b5d5712f906e20a16
pjbradbury <pjb39@cornell.edu>
2024-01-02 16:12:20 -0500

    adds path finding test (not finished)

--- src/test/kotlin/net/maizegenetics/phgv2/pathing/HaploidPathFindingTest.kt
+++ src/test/kotlin/net/maizegenetics/phgv2/pathing/HaploidPathFindingTest.kt
@@ -1,9 +1,13 @@
 package net.maizegenetics.phgv2.pathing
 
 import com.github.ajalt.clikt.testing.test
+import net.maizegenetics.phgv2.api.HaplotypeGraph
 import net.maizegenetics.phgv2.cli.TestExtension
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import java.io.File
+import kotlin.random.Random
+
 
 @ExtendWith(TestExtension::class)
 class HaploidPathFindingTest {
@@ -57,5 +61,56 @@ class HaploidPathFindingTest {
         assert(errOut.contains("Error: invalid value for --max-reads-per-kb: max-reads-per-kb must be a positive integer."))
         assert(errOut.contains("Error: invalid value for --min-coverage: min-coverage must be between 0.5 and 1.0"))
 
+    }
+
+    @Test
+    fun testPathFinding() {
+        //plan for this test:
+        //build a haplotype groph from Ref, lineA, and lineB
+        //for that need an hvcfdir with the files in it
+        val vcfDir = File(TestExtension.testVCFDir)
+        vcfDir.mkdirs()
+
+        //erase any files in testVCFDir
+        val vcfFiles = vcfDir.listFiles(){name -> name.endsWith("vcf")}
+        for (vcfFile in vcfFiles) {
+            vcfFile.delete()
+        }
+
+        //copy hvcf files to vcfDir
+        listOf(TestExtension.smallseqLineAHvcfFile, TestExtension.smallseqLineBHvcfFile, TestExtension.smallseqRefHvcfFile)
+            .forEach { filepath ->
+                val origFile = File(filepath)
+                origFile.copyTo(vcfDir.resolve(origFile.name))
+            }
+
+
+
+
+
+    }
+
+    fun getReadMappings(hvcfDir: File): Map<List<String>, Int> {
+        val probMapToA = 0.95
+        val probMapToOther = 0.5
+        val listOfHvcfFilenames = hvcfDir.listFiles().map { it.path }
+        val graph = HaplotypeGraph(listOfHvcfFilenames)
+        val readMap = mutableMapOf<List<String>, Int>()
+        for (range in graph.ranges()) {
+            val hapids = graph.hapIdToSampleGametes(range)
+            //generate some mappings
+            repeat(4) {
+                val hapidList = mutableListOf<String>()
+                for ((hapid, samples) in hapids.entries) {
+                    val isLineA = samples.any { it.name == "LineA" }
+
+                    if (isLineA && Random.nextDouble() < probMapToA) hapidList.add("LineA")
+                    if (!isLineA && Random.nextDouble() < probMapToOther) hapidList.add(samples[0].name)
+                }
+                if (hapidList.size > 0) readMap.put(hapidList, 2)
+            }
+        }
+
+        return readMap
     }
 }
