270a9c952ae8e2e2312372484f7b4672ea8447b1
lynnjo <lcj34@cornell.edu>
2024-01-30 11:01:14 -0500

    updated comments in VariantSetsService.kt

--- src/main/kotlin/net/maizegenetics/phgv2/brapi/service/VariantSetsService.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/brapi/service/VariantSetsService.kt
@@ -7,6 +7,12 @@ import net.maizegenetics.phgv2.brapi.model.VariantSetAvailableFormats
 import org.apache.logging.log4j.LogManager
 import java.io.File
 
+/**
+ * This class is used to generate the VariantSets object that is returned by the "variantsets" endpoint.
+ * It looks for a vcf file called allSamplesMerged.vcf.gz in the tiledb_uri directory.
+ * If it doesn't find it , one is created using the ExportVCF plugin.
+ * A URL pointing to the file is returned in the "availbleFormats" section of the VariantSets object.
+ */
 class VariantSetsService {
 
     private val myLogger = LogManager.getLogger(VariantSetsService::class.java)
@@ -24,7 +30,11 @@ class VariantSetsService {
         val variantSetURI = "${tildbUri}/variantsets/allSamplesMerged.vcf.gz"
         val availableFormats = VariantSetAvailableFormats(DataFormatEnum.VCF, fileFormat = FileFormatEnum.APPLICATION_ZIP, fileURL=variantSetURI)
         if (!File(variantSetURI).exists()) {
-            // create the file using Terry's code.
+            // TODO create and export the file using Terry's code.
+            // First create the graph, then call ExportVCF
+            // Are the hvcf files currently written to tiledb_uri/hvcf_files when they are created?
+            // Or do we need to export the hvcf files to tiledb_uri/hvcf_files and then create the graph
+            // from them?
         }
         // These values may not be correct, but the availableFormat.fileURI should be correct
         // What is our studDbId now?
