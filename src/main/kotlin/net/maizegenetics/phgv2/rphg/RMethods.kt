23975843fc4d99666983dada0675e032a2da659f
Brandon <brandon.monier@gmail.com>
2024-02-16 15:10:17 -0500

    Add tests and documentation

--- src/main/kotlin/net/maizegenetics/phgv2/rphg/RMethods.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/rphg/RMethods.kt
@@ -1,33 +1,51 @@
 package net.maizegenetics.phgv2.rphg
 
 import net.maizegenetics.phgv2.api.HaplotypeGraph
-import net.maizegenetics.phgv2.api.SampleGamete
-import javax.xml.crypto.Data
 
+/**
+ * Main class for interfacing PHGv2 data return methods with R
+ */
 class RMethods {
-    fun getFullHapIdMatrix(g: HaplotypeGraph): StringMatrix {
-
-        val allRanges = g.ranges()
-        val sampleGametes = g.sampleGametesInGraph()
-        val array2D = Array(g.samples().size) { Array(allRanges.size) { "" } }
+    /**
+     * Return a 2d haplotype ID matrix (sample gametes by ref range)
+     * from a HaplotypeGraph object
+     *
+     * @param hapGraph A `HaplotypeGraph` object
+     *
+     * @return A `StringMatrix` object that will be converted to a
+     *         `character` matrix in R
+     */
+    fun getHapIdMatrixFromGraph(hapGraph: HaplotypeGraph): RCharacterMatrix {
+        val allRanges = hapGraph.ranges()
+        val sampleGametes = hapGraph.sampleGametesInGraph()
+        val array2D = Array(hapGraph.samples().size) { Array(allRanges.size) { "" } }
 
         allRanges.forEach { range ->
-            val hapIdRange = g.sampleGameteToHaplotypeId(range)
-            hapIdRange.forEach {
-                array2D[sampleGametes.indexOf(it.key)][allRanges.indexOf(range)] = it.value
+            val hapIdRange = hapGraph.sampleGameteToHaplotypeId(range)
+            hapIdRange.forEach { sgEntry ->
+                array2D[sampleGametes.indexOf(sgEntry.key)][allRanges.indexOf(range)] = sgEntry.value
             }
         }
 
-        return StringMatrix (
+        return RCharacterMatrix (
             colNames = allRanges.map { "R$it" }.toTypedArray(),
             rowNames = sampleGametes.map { "${it.name}_G${it.gameteId + 1}" }.toTypedArray(),
             matrixData = array2D
         )
     }
 
-    fun getRefRanges(g: HaplotypeGraph): DataFrame {
-        val refRanges = g.ranges()
-        return DataFrame(
+    /**
+     * Return reference range data (contig, start, end) from a
+     * HaplotypeGraph object
+     *
+     * @param hapGraph A `HaplotypeGraph` object
+     *
+     * @return A `RPHGList` object that will be converted to a
+     *         `GenomicRanges` object in R
+     */
+    fun getRefRangesFromGraph(hapGraph: HaplotypeGraph): RList {
+        val refRanges = hapGraph.ranges()
+        return RList(
             colNames = arrayOf("seqname", "start", "end"),
             rowNames = null,
             matrixData = arrayOf(
@@ -37,49 +55,50 @@ class RMethods {
             )
         )
     }
-    
-    fun testStringMatrix(): StringMatrix {
-        return MatrixWithNames(
-            colNames = arrayOf("Col1", "Col2", "Col3"),
-            rowNames = arrayOf("Row1", "Row2", "Row3", "Row4"),
+
+    /**
+     * Return haplotype ID metadata (alt headers) from a
+     * HaplotypeGraph object
+     *
+     * @param hapGraph A `HaplotypeGraph` object
+     *
+     * @return A `RPHGList` object that will be converted to a
+     *         `data.frame` object in R
+     */
+    fun getAltHeadersFromGraph(hapGraph: HaplotypeGraph): RList {
+        val altHeaders = hapGraph.altHeaders()
+
+        val positions = altHeaders.map { it.value.regions.first() }
+
+        val contigStart = positions.map { it.first.contig }
+        val contigEnd = positions.map { it.second.contig }
+        val start = positions.map { it.first.position }
+        val end = positions.map { it.second.position }
+
+        return RList(
+            colNames = arrayOf(
+                "hap_id",
+                "sample_name",
+                "description",
+                "source",
+                "checksum",
+                "contig_start",
+                "contig_end",
+                "start",
+                "end",
+                "ref_range_hash"
+            ),
             matrixData = arrayOf(
-                arrayOf("a", "b", "c"),
-                arrayOf("d", "e", "f"),
-                arrayOf("g", "h", "i"),
-                arrayOf("j", "k", "l")
-            )
-        )
-    }
-
-    fun testIntMatrix(): IntMatrix {
-        return MatrixWithNames (
-            arrayOf("i1", "i2", "i3", "i4"),
-            arrayOf("r1", "r2"),
-            arrayOf(
-                intArrayOf(1, 2, 3, 4),
-                intArrayOf(5, 6, 7, 8)
-            )
-        )
-    }
-
-    fun testDblMatrix(): DoubleMatrix {
-        return MatrixWithNames(
-            arrayOf("d1", "d2", "d3", "d4"),
-            arrayOf("r1", "r2"),
-            arrayOf(
-                doubleArrayOf(1.3, 2.1, 3.1434, 4.2),
-                doubleArrayOf(5.1, 6.234, 7.0, 8.67)
-            )
-        )
-    }
-
-    fun testDataFrame(): DataFrame {
-        return MatrixWithNames(
-            arrayOf("df1", "df2", "df3", "df4"),
-            arrayOf("r1", "r2"),
-            arrayOf(
-                arrayOf(1, 2, 3),
-                arrayOf("this", "is", "category")
+                altHeaders.map { it.value.id }.toTypedArray(),
+                altHeaders.map { it.value.sampleName }.toTypedArray(),
+                altHeaders.map { it.value.description }.toTypedArray(),
+                altHeaders.map { it.value.source }.toTypedArray(),
+                altHeaders.map { it.value.checksum }.toTypedArray(),
+                contigStart.toTypedArray(),
+                contigEnd.toTypedArray(),
+                start.toTypedArray(),
+                end.toTypedArray(),
+                altHeaders.map { it.value.refRange }.toTypedArray()
             )
         )
     }
