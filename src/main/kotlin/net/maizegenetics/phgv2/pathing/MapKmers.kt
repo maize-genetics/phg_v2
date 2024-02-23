3029c16acfe3cc8de5831be5850aa2e83bdd25e4
Zack Miller <zrm22@cornell.edu>
2024-02-23 08:53:28 -0500

    Fixing relative path issue we noticed during scale testing.

--- src/main/kotlin/net/maizegenetics/phgv2/pathing/MapKmers.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/pathing/MapKmers.kt
@@ -91,7 +91,7 @@ class MapKmers : CliktCommand(help="Map Kmers to the pangenome reference") {
         myLogger.info("Begin mapping reads to the pangenome kmer index.")
         //loop through all files in hvcfDir and create a list of hvcf files
         val hvcfFiles = File(hvcfDir).walkTopDown().filter { it.isFile }
-            .filter { it.name.endsWith("h.vcf") || it.name.endsWith("h.vcf.gz") }.map { "${it.path}/${it.name}" }
+            .filter { it.name.endsWith("h.vcf") || it.name.endsWith("h.vcf.gz") }.map { "${hvcfDir}/${it.name}" }
             .toList()
 
         //create a HaplotypeGraph from the list of hvcf files
