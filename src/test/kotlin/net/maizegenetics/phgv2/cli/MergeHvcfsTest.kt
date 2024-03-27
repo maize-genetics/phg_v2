f9a2aa3c78330b4bd669873cd05acd467cb35582
Terry Casstevens <tmc46@cornell.edu>
2024-03-27 16:28:36 -0400

    Initial implementation of clikt command merge-hvcfs

---
+++ src/test/kotlin/net/maizegenetics/phgv2/cli/MergeHvcfsTest.kt
@@ -0,0 +1,64 @@
+package net.maizegenetics.phgv2.cli
+
+import com.github.ajalt.clikt.testing.test
+import net.maizegenetics.phgv2.utils.getChecksum
+import org.apache.logging.log4j.LogManager
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.io.File
+import kotlin.test.assertEquals
+
+@ExtendWith(TestExtension::class)
+class MergeHvcfsTest {
+
+    private val myLogger = LogManager.getLogger(MergeHvcfsTest::class.java)
+
+    companion object {
+
+        private val exportHvcfDir = "${TestExtension.tempDir}/merge-hvcfs/"
+        private val singleInputDir = "${TestExtension.tempDir}/single-input/"
+        private val multiInputDir = "${TestExtension.tempDir}/multi-input/"
+        private val outputHvcfDir = "${exportHvcfDir}/output/"
+
+        @BeforeAll
+        @JvmStatic
+        fun setup() {
+            File(exportHvcfDir).mkdirs()
+            File(outputHvcfDir).mkdirs()
+            File(singleInputDir).mkdirs()
+
+            File(TestExtension.smallseqRefHvcfFile).copyTo(File(singleInputDir + File(TestExtension.smallseqRefHvcfFile).name))
+
+            File(TestExtension.smallseqRefHvcfFile).copyTo(File(multiInputDir + File(TestExtension.smallseqRefHvcfFile).name))
+            File(TestExtension.smallseqLineAHvcfFile).copyTo(File(multiInputDir + File(TestExtension.smallseqLineAHvcfFile).name))
+            File(TestExtension.smallseqLineBHvcfFile).copyTo(File(multiInputDir + File(TestExtension.smallseqLineBHvcfFile).name))
+        }
+
+    }
+
+    @Test
+    fun testSingleSampleMergeHvcfs() {
+
+        val outputFile = "${outputHvcfDir}testSingleSampleHaplotypeGraph.vcf"
+
+        // phg merge-hvcfs --input-dir <singleInputDir> --id-format CHECKSUM --reference-file TestExtension.smallseqRefFile --output-file exported-vcfs
+        val result = MergeHvcfs().test(
+            "--input-dir $singleInputDir --id-format CHECKSUM --reference-file ${TestExtension.smallseqRefFile} --output-file $outputFile"
+        )
+
+        myLogger.info("testSingleSampleMergeHvcfs: result output: ${result.output}")
+
+        assertEquals(result.statusCode, 0, "status code not 0: ${result.statusCode}")
+
+        var checksum1 = getChecksum(TestExtension.exportGraphSingleSample)
+        var checksum2 = getChecksum(outputFile)
+
+        myLogger.info("testSingleSampleHaplotypeGraph.vcf expected checksum1: $checksum1")
+        myLogger.info("testSingleSampleHaplotypeGraph.vcf actual checksum2: $checksum2")
+
+        assertEquals(checksum1, checksum2, "testSingleSampleHaplotypeGraph.vcf checksums do not match")
+
+    }
+    
+}
