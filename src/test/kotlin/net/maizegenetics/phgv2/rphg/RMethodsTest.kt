3b6de907a80c5cd70ca3beeb03217e5ceda0f06f
Brandon <brandon.monier@gmail.com>
2024-04-15 11:40:25 -0500

    Add identifier column

--- src/test/kotlin/net/maizegenetics/phgv2/rphg/RMethodsTest.kt
+++ src/test/kotlin/net/maizegenetics/phgv2/rphg/RMethodsTest.kt
@@ -142,17 +142,18 @@ class RMethodsTest {
         val testRefRanges = rMethods.getRefRangesFromGraph(graph)
 
         assertEquals(
-            listOf("seqname", "start", "end"),
+            listOf("seqname", "start", "end", "rr_id"),
             testRefRanges.colNames?.toList()
         )
 
         assertEquals("phgv2_r_list", testRefRanges.toString())
         assertEquals(40, testRefRanges.matrixData!![0].size)
-        assertEquals(3, testRefRanges.matrixData!!.size)
+        assertEquals(4, testRefRanges.matrixData!!.size)
         assert(testRefRanges.rowNames.isNullOrEmpty())
         assert(testRefRanges.matrixData!![0].isArrayOf<String>())
         assert(testRefRanges.matrixData!![1].isArrayOf<Int>())
         assert(testRefRanges.matrixData!![2].isArrayOf<Int>())
+        assert(testRefRanges.matrixData!![3].isArrayOf<String>())
     }
 
     @Test
