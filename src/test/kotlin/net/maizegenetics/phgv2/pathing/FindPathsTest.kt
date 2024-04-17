1463d1c348cbdebf4d3ad2488e75205e2e8aaa33
pjbradbury <pjb39@cornell.edu>
2024-04-17 13:52:16 -0400

    uncomment tearDown in FindPathsTest

--- src/test/kotlin/net/maizegenetics/phgv2/pathing/FindPathsTest.kt
+++ src/test/kotlin/net/maizegenetics/phgv2/pathing/FindPathsTest.kt
@@ -51,7 +51,7 @@ class FindPathsTest {
         @AfterAll
         fun tearDown() {
             //comment out the following line to inspect the test results after the tests have been run
-//            File(TestExtension.testOutputDir).deleteRecursively()
+            File(TestExtension.testOutputDir).deleteRecursively()
         }
     }
 
