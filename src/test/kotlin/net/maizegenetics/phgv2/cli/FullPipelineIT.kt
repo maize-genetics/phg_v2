77f00b3dc25b23786e6edf8288adbad1b7f9f3ff
lynnjo <lcj34@cornell.edu>
2024-05-28 07:21:10 -0400

    reverting FullPipelineIT.kt to main for merging

--- src/test/kotlin/net/maizegenetics/phgv2/cli/FullPipelineIT.kt
+++ src/test/kotlin/net/maizegenetics/phgv2/cli/FullPipelineIT.kt
@@ -2,6 +2,7 @@ package net.maizegenetics.phgv2.cli
 
 import biokotlin.seqIO.NucSeqIO
 import com.github.ajalt.clikt.testing.test
+import htsjdk.variant.vcf.VCFFileReader
 import net.maizegenetics.phgv2.api.HaplotypeGraph
 import net.maizegenetics.phgv2.api.SampleGamete
 import net.maizegenetics.phgv2.cli.TestExtension.Companion.asmList
@@ -60,6 +61,7 @@ class FullPipelineIT {
             File(TestExtension.testInputFastaDir).mkdirs()
             File(TestExtension.testOutputFastaDir).mkdirs()
             File(TestExtension.testOutputHVCFDir).mkdirs()
+            File(TestExtension.testOutputGVCFDir).mkdirs()
             File(TestExtension.testTileDBURI).mkdirs()
             File(TestExtension.testOutputDir).mkdirs()
         }
@@ -80,9 +82,30 @@ class FullPipelineIT {
         val createRanges = CreateRanges()
         val createRangesResult = createRanges.test("--gff ${TestExtension.smallseqAnchorsGffFile} --output ${TestExtension.testBEDFile} --reference-file ${TestExtension.smallseqRefFile}")
 
+        //Write fastas to testInputFastaDir, changing chromosome names for LineB in order to test
+        // that the full pipeline will work when assembly chromosome names are not all the same
+        File(TestExtension.smallseqLineAFile).copyTo(File("${TestExtension.testInputFastaDir}LineA.fa"))
+        getBufferedWriter("${TestExtension.testInputFastaDir}LineB.fa").use { fastaWriter ->
+            getBufferedReader(TestExtension.smallseqLineBFile).use { fastaReader ->
+                fastaReader.forEachLine {
+                    val newLine = it.replace(">", ">chr")
+                    fastaWriter.write(newLine)
+                    fastaWriter.write("\n")
+                }
+            }
+        }
+
+        //write the assemblies list
+        val assembliesList = "${TestExtension.testInputFastaDir}assembliesList.txt"
+        getBufferedWriter(assembliesList).use { myWriter ->
+            myWriter.write("${TestExtension.testInputFastaDir}LineA.fa\n")
+            myWriter.write("${TestExtension.testInputFastaDir}LineB.fa\n")
+        }
+
         //Create the agc record:
         val agcCompress = AgcCompress()
-        val agcResult = agcCompress.test("--fasta-list ${TestExtension.smallseqAssembliesListFile} --db-path ${TestExtension.testTileDBURI} --reference-file ${TestExtension.smallseqRefFile}")
+
+        val agcResult = agcCompress.test("--fasta-list $assembliesList --db-path ${TestExtension.testTileDBURI} --reference-file ${TestExtension.smallseqRefFile}")
         println(agcResult.output)
 
         println(createRangesResult.output)
@@ -95,7 +118,7 @@ class FullPipelineIT {
         val alignAssemblies = AlignAssemblies()
         val alignAssembliesResult = alignAssemblies.test(
             "--gff ${TestExtension.smallseqAnchorsGffFile} --reference-file ${TestExtension.smallseqRefFile} " +
-                    "--assembly-file-list ${TestExtension.smallseqAssembliesListFile} -o ${TestExtension.testMafDir}"
+                    "-a $assembliesList -o ${TestExtension.testMafDir}"
         )
         println(alignAssembliesResult.output)
 
@@ -130,7 +153,6 @@ class FullPipelineIT {
             assertEquals(id, seqHash, "Hashes do not match for $id")
         }
 
-
         //build a composite genome from the HVCFs
         createFastaFromHvcf.test("--db-path ${TestExtension.testTileDBURI} --fasta-type composite --hvcf-file ${TestExtension.testOutputHVCFDir}/Ref.h.vcf -o ${TestExtension.testOutputFastaDir}/Ref_composite.fa")
         createFastaFromHvcf.test("--db-path ${TestExtension.testTileDBURI} --fasta-type composite --hvcf-file ${TestExtension.testOutputHVCFDir}/LineA.h.vcf -o ${TestExtension.testOutputFastaDir}/LineA_composite.fa")
@@ -146,13 +168,37 @@ class FullPipelineIT {
             assertTrue(asmDiff < 0.00001, "${asmName} Fasta is not the same as input")
         }
 
+        //export select sites as a gvcf
+        val bedResult = ExportVcf().test(
+            "--db-path ${TestExtension.testTileDBURI} -o ${TestExtension.testOutputGVCFDir} --dataset-type gvcf --sample-names LineA,LineB " +
+                    "--regions-file ${TestExtension.smallSeqLineAPositionsBed}"
+        )
+        assertEquals(0, bedResult.statusCode, "gvcf export not successful")
+
+        //check gvcf export with a bedfile for positions
+        val bedRecords = getBufferedReader(TestExtension.smallSeqLineAPositionsBed).use { bedReader ->
+            bedReader.readLines()
+        }
+        checkExpectedGvcf("${TestExtension.testOutputGVCFDir}LineA.g.vcf", bedRecords)
+        checkExpectedGvcf("${TestExtension.testOutputGVCFDir}LineB.g.vcf", bedRecords)
+        File("${TestExtension.testOutputGVCFDir}LineA.g.vcf").delete()
+        File("${TestExtension.testOutputGVCFDir}LineB.g.vcf").delete()
+
+        //repeat the gvcf export using a vcf file for positions
+        val vcfResult = ExportVcf().test(
+            "--db-path ${TestExtension.testTileDBURI} -o ${TestExtension.testOutputGVCFDir} --dataset-type gvcf --sample-names LineA,LineB " +
+                    "--regions-file ${TestExtension.smallSeqLineAPositionsVcf}"
+        )
+        assertEquals(0, vcfResult.statusCode, "gvcf export not successful")
+        checkExpectedGvcf("${TestExtension.testOutputGVCFDir}LineA.g.vcf", bedRecords)
+        checkExpectedGvcf("${TestExtension.testOutputGVCFDir}LineB.g.vcf", bedRecords)
+
         //build a kmer index
         println("building kmer index")
         val buildKmerIndexArgs = "--db-path ${TestExtension.testTileDBURI} --hvcf-dir ${TestExtension.testVCFDir}"
         val indexResult = BuildKmerIndex().test(buildKmerIndexArgs)
         assertEquals(0, indexResult.statusCode, "Kmer Indexing failed")
         println(indexResult.output)
-
 
         //create some reads with fairly high coverage to make sure mapping results are consistent so that they can be tested.
         val lineAFastqFilename = TestExtension.testInputFastaDir + "readsLineA.fastq"
@@ -286,6 +332,25 @@ class FullPipelineIT {
             if (doNotTestLastRange && range.start < 50500)
             assertEquals(graph.sampleToHapId(range, sgA), graph.sampleToHapId(range, sgTestSample), "TestSample hapid does not equal line A in $range")
         }
+    }
+
+    fun checkExpectedGvcf(testGvcf: String, bedRecords: List<String>) {
+        //Each position in bedRecords should have a record in the testGvcf
+        //But, more than one bed position may be covered by the same
+        var bedIndex = 0
+        val bedPositions = bedRecords.map { it.split("\t")[2].toInt() }
+        VCFFileReader(File(testGvcf), false).use { vcfReader ->
+            vcfReader.forEach { variantContext ->
+                val vcfStart = variantContext.start
+                val vcfEnd = variantContext.end
+                val bedPos = bedPositions[bedIndex]
+                assertTrue(bedPos in vcfStart..vcfEnd, "bed position $bedPos not in vcf range of $vcfStart to $vcfEnd")
+
+                //advance the bedIndex until the position is no longer in the current gvcf interval
+                while (bedIndex < bedPositions.size && bedPositions[bedIndex] in vcfStart..vcfEnd) bedIndex++
+            }
+        }
+
     }
 
 }
