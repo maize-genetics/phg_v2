c8ea12633dacfbb30ca87570340a1324462cbebb
lynnjo <lcj34@cornell.edu>
2024-05-28 07:49:47 -0400

    committing merged FullPipelineIT.kt

--- src/test/kotlin/net/maizegenetics/phgv2/cli/FullPipelineIT.kt
+++ src/test/kotlin/net/maizegenetics/phgv2/cli/FullPipelineIT.kt
@@ -105,7 +105,7 @@ class FullPipelineIT {
         //Create the agc record:
         val agcCompress = AgcCompress()
 
-        val agcResult = agcCompress.test("--fasta-list $assembliesList --db-path ${TestExtension.testTileDBURI} --reference-file ${TestExtension.smallseqRefFile}")
+        val agcResult = agcCompress.test("--fasta-list ${TestExtension.smallseqAssembliesListFile} --db-path ${TestExtension.testTileDBURI} --reference-file ${TestExtension.smallseqRefFile}")
         println(agcResult.output)
 
         println(createRangesResult.output)
@@ -118,7 +118,7 @@ class FullPipelineIT {
         val alignAssemblies = AlignAssemblies()
         val alignAssembliesResult = alignAssemblies.test(
             "--gff ${TestExtension.smallseqAnchorsGffFile} --reference-file ${TestExtension.smallseqRefFile} " +
-                    "-a $assembliesList -o ${TestExtension.testMafDir}"
+                    "--assembly-file-list ${TestExtension.smallseqAssembliesListFile} -o ${TestExtension.testMafDir}"
         )
         println(alignAssembliesResult.output)
 
