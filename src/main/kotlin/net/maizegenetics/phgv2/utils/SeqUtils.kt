15435e5290966ab7addc0e7508e054976a59563e
lynnjo <lcj34@cornell.edu>
2024-07-10 16:16:16 -0400

    Fixed pulling chrom name from fasta idLine

--- src/main/kotlin/net/maizegenetics/phgv2/utils/SeqUtils.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/utils/SeqUtils.kt
@@ -43,9 +43,38 @@ private val myLogger = LogManager.getLogger("net.maizegenetics.phgv2.utils.SeqUt
  *      e.g. LineA:1,2
  *           LineB:1,2
  *
+ * 4. retrieveRefSampleName: called to get the name of the sample used as the reference in the AGC file.
+ *    Expected user input: dbPath:String
+ *    THe command sent to age is "agc listref <in.agc>"  THe output is written to the output stream
+ *    and returned from the function as a string.
+ *
  * The rest of the functions in this file are used internally by the above functions.
  *
  */
+
+/**
+ * Retrieves the name of the sample used as the reference in the AGC file.
+ * @param dbPath    the folder containing assemblies.agc
+ * @return the name of the sample used as the reference in the AGC file
+ */
+fun retrieveRefSampleName(dbPath: String,condaEnvPrefix:String):String {
+    val agcFile = "${dbPath}/assemblies.agc"
+    check(File(agcFile).exists()) { "File assemblies.agc does not exist in folder $dbPath- please send a valid path that indicates the parent folder for your assemblies.agc compressed file." }
+
+    val command = if (condaEnvPrefix.isNotBlank()) arrayOf("conda","run","-p",condaEnvPrefix,"agc","listref",agcFile)
+    else arrayOf("conda","run","-n","phgv2-conda","agc","listref",agcFile)
+
+    val commandToPrint = if (command.joinToString(" ").length > 150) command.joinToString(" ").substring(0,150) else command.joinToString(" ")
+    myLogger.info("retrieveRefSampleName: Running Agc Command:\n${commandToPrint}...")
+
+    val agcProcess = ProcessBuilder(*command)
+        .start()
+
+    val agcOut = BufferedInputStream(agcProcess.inputStream, 5000000)
+    val refSampleName = agcOut.bufferedReader().readLine()
+    agcOut.close()
+    return refSampleName
+}
 
 /**
  * Retrieves sequence from an agc data store.
@@ -309,7 +338,8 @@ fun queryAgc(commands:Array<String>):Map<Pair<String,String>,NucSeq> {
                         genomeChromNucSeq.put(Pair(currGenome,chromPlusRange),NucSeq(currSeq.toString()))
                     }
                     // reset chromosome name and sequence, begin processing next chrom
-                    currChrom = line.substring(1).split(" ")[0]
+                    // Split on ALL whitespace, to handle tabs etc between the chrom name and other data on the line
+                    currChrom = line.substring(1).split("\\s+".toRegex())[0]
 
                     val genomeStart = line.indexOf("sampleName=")
                     if (genomeStart < 0) {
