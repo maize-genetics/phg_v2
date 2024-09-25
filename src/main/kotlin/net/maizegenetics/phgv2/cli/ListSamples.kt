cdd68eaecd769c5d4e97a7799994dd89ef23bc99
lynnjo <lcj34@cornell.edu>
2024-09-25 11:32:07 -0400

    updated based on Terry's comments

--- src/main/kotlin/net/maizegenetics/phgv2/cli/ListSamples.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/cli/ListSamples.kt
@@ -1,6 +1,7 @@
 package net.maizegenetics.phgv2.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.convert
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
@@ -8,21 +9,47 @@ import net.maizegenetics.phgv2.utils.getBufferedWriter
 import net.maizegenetics.phgv2.utils.retrieveAgcData
 import net.maizegenetics.phgv2.utils.verifyURI
 import org.apache.logging.log4j.LogManager
+import kotlin.test.DefaultAsserter.fail
 
 /**
  * This class is used to list the sample names in both the AGC compressed file and the tiledb datasets.
  * It is a command that allows users to see what data they have loaded, and make decisions on
  * which sample data to include when imputing, analysing or exporting data.
  *
- * It takes as input a path to the tiledb datasets and an output file.  Data on sample names
- * is written to a tab-delimited file with columns for the sample name, and whether the sample
- * is in the AGC file, the gvcf dataset and/or the hvcf dataset.
+ * It takes as input a path to the tiledb datasets, a dataset type for output,  and an output file.
+ * By default, the output will be a file, one sample per line, showing
+ * the sample names that exist in the hvcf dataset.
+ *
+ * The optional parameter --data-set, allows the user to specify the dataset from which to pull sample names.  The default is hvcf.
+ * The options are agc, gvcf, hvcf, and all.  If all is selected, the samples from all datasets are listed in a tab-delimited file
+ * with columns for the sample name, and whether the sample is in the AGC file, the gvcf dataset and/or the hvcf dataset.
+ *
+ * If "hvcf", "gvcf" or "agc" is selected, the output will be a file with a list of sample names in the selected dataset, one per line
+ *
+ * Another optional parameter is the conda environment prefix.  If provided, this should be the full path to the conda environment.
+ *
  */
+
+// Create an enum class to hold the dataset options
+enum class DatasetOptions {
+    agc, gvcf, hvcf, all
+}
 class ListSamples: CliktCommand(help = "List the sample names in both the AGC compressed file and the tiledb datasets.") {
     private val myLogger = LogManager.getLogger(ListSamples::class.java)
 
     val dbPath by option(help = "Folder name where TileDB datasets and AGC record is stored.  If not provided, the current working directory is used")
         .default("")
+
+    // Verify the dataset option is one of the four options
+    val dataSet by option(help = "Name of the dataset to list the samples from. Options are: all, agc, gvcf, hvcf. Default is hvcf.")
+        .convert {
+            try {
+                DatasetOptions.valueOf(it)
+            } catch (IAE: IllegalArgumentException) {
+                fail("Invalid dataset option. Available options are: ${DatasetOptions.values().joinToString(", ")}")
+            }
+        }
+        .default(DatasetOptions.hvcf)
 
     val condaEnvPrefix by option (help = "Prefix for the conda environment to use.  If provided, this should be the full path to the conda environment.")
         .default("")
@@ -38,42 +65,76 @@ class ListSamples: CliktCommand(help = "List the sample names in both the AGC co
 
         myLogger.info("URI verified: $tileDb , calling createSampleLists")
         // call command to get list of samples from the AGC file and both tiledb datasets
-        createSampleLists(tileDb, condaEnvPrefix, outputFile)
+        createSampleLists(tileDb, condaEnvPrefix, outputFile, dataSet)
     }
 
     // Function to create the output file with the list of samples in the AGC file and the TileDB datasets
     // There may be some samples in the hvcf that are not in the gvcf, in particular the reference genome
     // We would expect that otherwise the samples in the gvcf and hvcf datasets would be the same but it
     // is possible users have only loaded a portion of each type vcf.
-    fun createSampleLists(dbPath: String, condaEnvPrefix: String, outputFile: String) {
+    fun createSampleLists(dbPath: String, condaEnvPrefix: String, outputFile: String, dataset:DatasetOptions) {
+
+
         // call command to get list of samples from the AGC file
         myLogger.info("Getting agc sample list")
-        val agcSamples = retrieveAgcData(dbPath,listOf("listset"),condaEnvPrefix).sorted()
+        val agcSamples = if (dataset == DatasetOptions.agc || dataset == DatasetOptions.all) retrieveAgcData(dbPath,listOf("listset"),condaEnvPrefix).sorted()
+            else emptyList()
 
         // call command to get list of samples from the TileDB datasets, first from hvcf_dataset
         var uri = dbPath + "/gvcf_dataset"
         myLogger.info("Getting gvcf sample list")
-        val gvcfSamples = LoadVcf().getTileDBSampleLists(uri).sorted()
+        val gvcfSamples = if (dataset == DatasetOptions.gvcf || dataset == DatasetOptions.all) LoadVcf().getTileDBSampleLists(uri).sorted() else emptyList()
 
         uri = dbPath + "/hvcf_dataset"
         myLogger.info("Getting hvcf sample list")
-        val hvcfSamples = LoadVcf().getTileDBSampleLists(uri).sorted()
+        val hvcfSamples = if (dataset == DatasetOptions.hvcf || dataset == DatasetOptions.all) LoadVcf().getTileDBSampleLists(uri).sorted() else emptyList()
 
         // take the agcSamples, gvcfSamples and hvcfSamples and create a union of the three lists
         val allSamples = agcSamples.union(gvcfSamples).union(hvcfSamples).sorted()
         val headerLine = "SampleName\tInAGC\tInGVCF\tInHVCF\n"
 
-        // write the sample list to the output file.
-        getBufferedWriter(outputFile).use { writer ->
-            writer.write(headerLine)
-            for (sample in allSamples) {
-                val inAGC = if (agcSamples.contains(sample)) "Y" else "N"
-                val inGVCF = if (gvcfSamples.contains(sample)) "Y" else "N"
-                val inHVCF = if (hvcfSamples.contains(sample)) "Y" else "N"
-                writer.write("$sample\t$inAGC\t$inGVCF\t$inHVCF\n")
+        // Create outputFile based on the dataset option
+        when (dataset) {
+            DatasetOptions.agc -> {
+                // write the values from the agcSamples list to the output file, one per line.
+                getBufferedWriter(outputFile).use { writer ->
+                    for (sample in agcSamples) {
+                        writer.write("$sample\n")
+                    }
+                }
+            }
+            DatasetOptions.gvcf -> {
+                // write the values from the gvcfSamples list to the output file, one per line.
+                getBufferedWriter(outputFile).use { writer ->
+                    for (sample in gvcfSamples) {
+                        writer.write("$sample\n")
+                    }
+                }
+            }
+            DatasetOptions.hvcf ->  {
+                // write the values from the hvcfSamples list to the output file, one per line.
+                getBufferedWriter(outputFile).use { writer ->
+                    for (sample in hvcfSamples) {
+                        writer.write("$sample\n")
+                    }
+                }
+            }
+            DatasetOptions.all -> {
+                // take the agcSamples, gvcfSamples and hvcfSamples and create a union of the three lists
+                val allSamples = agcSamples.union(gvcfSamples).union(hvcfSamples).sorted()
+                val headerLine = "SampleName\tInAGC\tInGVCF\tInHVCF\n"
+                // write the sample list to the output file.
+                getBufferedWriter(outputFile).use { writer ->
+                    writer.write(headerLine)
+                    for (sample in allSamples) {
+                        val inAGC = if (agcSamples.contains(sample)) "Y" else "N"
+                        val inGVCF = if (gvcfSamples.contains(sample)) "Y" else "N"
+                        val inHVCF = if (hvcfSamples.contains(sample)) "Y" else "N"
+                        writer.write("$sample\t$inAGC\t$inGVCF\t$inHVCF\n")
+                    }
+                }
             }
         }
-
-        myLogger.info("Sample list data written to $outputFile")
+        myLogger.info("Sample list data for ${dataset} written to $outputFile")
     }
 }
--- src/test/kotlin/net/maizegenetics/phgv2/cli/ListSamplesTest.kt
+++ src/test/kotlin/net/maizegenetics/phgv2/cli/ListSamplesTest.kt
@@ -5,8 +5,8 @@ import com.google.common.io.Files
 import net.maizegenetics.phgv2.brapi.createSmallSeqTiledb
 import net.maizegenetics.phgv2.brapi.resetDirs
 import net.maizegenetics.phgv2.utils.bgzipAndIndexGVCFfile
-import org.apache.logging.log4j.LogManager
 import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -37,22 +37,38 @@ class ListSamplesTest {
     }
 
     @Test
-    fun testRunningListSamples() {
+    fun testRunningListSamplesAll() {
+        // all and gvcf are tested together because the "beforeAll" sets up the database
+        // with only hvcf files.  This db persists through all the tests.  Initially there
+        // are no gvcf files in the db.  We test, then add a gvcf filea and test again.
+        // If the entire suite of tests in this file are run at once, the db for subsequent
+        // tests has the gvcf file in it, making testing with/without gvcf samples more time intensive
+        // as it would be necessary to delete from the db.  But that would fail if the test
+        // was then run individually.  So, the tests are run in two groups, all/gvcf and hvcf.
         val outputFile = "${TestExtension.tempDir}list_samples.txt"
         var result = ListSamples().test(
-            "--db-path ${TestExtension.testTileDBURI} --output-file $outputFile"
+            "--db-path ${TestExtension.testTileDBURI} --output-file $outputFile --data-set all"
         )
-        assertEquals(result.statusCode, 0)
+        assertEquals(0,result.statusCode)
 
         // Read the output file.
         // Verify the file has a header line with columns SampleName, InAGC, InGVCF, InHVCF
         // Verify the file Has a "Y" for all samples in the InAGC and InHVCF columns, and "N" for all samples in the InGVCF column.
-        val outFileLines = File(outputFile).readLines()
+        var outFileLines = File(outputFile).readLines()
         assertEquals(outFileLines.size, 4)
         assertEquals(outFileLines[0], "SampleName\tInAGC\tInGVCF\tInHVCF")
         assertEquals(outFileLines[1], "LineA\tY\tN\tY")
         assertEquals(outFileLines[2], "LineB\tY\tN\tY")
         assertEquals(outFileLines[3], "Ref\tY\tN\tY")
+
+        // Test the gvcf option
+        val outputFileGvcf = "${TestExtension.tempDir}sampleGvcf.txt"
+        result = ListSamples().test(
+            "--db-path ${TestExtension.testTileDBURI} --output-file $outputFileGvcf --data-set gvcf"
+        )
+        outFileLines = File(outputFileGvcf).readLines()
+        assertEquals(0,result.statusCode)
+        assertEquals(0, outFileLines.size) // no gvcf samples yet
 
         // On setup, we called createSmallSeqTiledb() which calls loadVcfFiles(tiledbURI)
         // This is from the brapiSetup code, and it only loads LineA.h.vcf and LineB.h.vcf
@@ -73,9 +89,9 @@ class ListSamplesTest {
         assertEquals(result.statusCode, 0)
 
         val outputFile2 = "${TestExtension.tempDir}list_samples2.txt"
-        // Re-run ListSamples command.  LineA should now have Y in the InGVCF column.
+        // Re-run ListSamples command for "all".  LineA should now have Y in the InGVCF column.
         result = ListSamples().test(
-            "--db-path ${TestExtension.testTileDBURI} --output-file $outputFile2"
+            "--db-path ${TestExtension.testTileDBURI} --output-file $outputFile2 --data-set all"
         )
         assertEquals(result.statusCode, 0)
 
@@ -87,6 +103,64 @@ class ListSamplesTest {
         assertEquals(outFileLines2[2], "LineB\tY\tN\tY")
         assertEquals(outFileLines2[3], "Ref\tY\tN\tY")
 
-        println("testRunningListSamples: result output: ${result.output}")
+        // re-run the gvcf option
+        val outputFileGvcf2 = "${TestExtension.tempDir}sampleGvcf2.txt"
+        result = ListSamples().test(
+            "--db-path ${TestExtension.testTileDBURI} --output-file $outputFileGvcf2 --data-set gvcf"
+        )
+        outFileLines = File(outputFileGvcf2).readLines()
+        assertEquals(0,result.statusCode)
+        assertEquals(1, outFileLines.size) // LineA should now be in the gvcf file
+        assertEquals(outFileLines[0], "LineA")
+
+    }
+
+    @Test
+    fun testListSamplesForHvcf() {
+        val outputFile = "${TestExtension.tempDir}listHvcfSamples.txt"
+        var result = ListSamples().test(
+            "--db-path ${TestExtension.testTileDBURI} --output-file $outputFile --data-set hvcf"
+        )
+        assertEquals(0,result.statusCode)
+
+        val outFileLines = File(outputFile).readLines()
+        assertEquals(outFileLines.size, 3)
+        assertEquals(outFileLines[0], "LineA")
+        assertEquals(outFileLines[1], "LineB")
+        assertEquals(outFileLines[2], "Ref")
+    }
+
+    @Test
+    fun testListSamplesDefault() {
+        val outputFile = "${TestExtension.tempDir}listHvcfSamples.txt"
+        var result = ListSamples().test(
+            "--db-path ${TestExtension.testTileDBURI} --output-file $outputFile "
+        )
+        assertEquals(0,result.statusCode)
+
+        // Should run and print the samples in the hvcf file (which is the default)
+        val outFileLines = File(outputFile).readLines()
+        assertEquals(outFileLines.size, 3)
+        assertEquals(outFileLines[0], "LineA")
+        assertEquals(outFileLines[1], "LineB")
+        assertEquals(outFileLines[2], "Ref")
+    }
+
+    @Test
+    fun testBadDatasetOption() {
+        val outputFile = "${TestExtension.tempDir}listHvcfSamples.txt"
+        var result = ListSamples().test(
+            "--db-path ${TestExtension.testTileDBURI} --output-file $outputFile --data-set bad"
+        )
+        assertEquals(1,result.statusCode)
+
+        // Cannot get this error message correct !!!!
+//        assertEquals("Usage: list-samples [<options>]\n" +
+//                "\n" ,result.output)
+//        assertEquals("Usage: list-samples [<options>]\n" +
+//                "\n" +
+//                "Error: invalid value for --data-set: Invalid dataset option. Available options are: agc, gvcf, hvcf, all\n",result.output)
+
+
     }
 }
