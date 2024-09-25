091b1fa80b02c485199452ad0d330373180def76
lynnjo <lcj34@cornell.edu>
2024-09-25 15:11:47 -0400

    temporarily revert Phg.kt for merge with main

--- src/main/kotlin/net/maizegenetics/phgv2/cli/Phg.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/cli/Phg.kt
@@ -7,7 +7,6 @@ import com.github.ajalt.clikt.output.MordantHelpFormatter
 import com.github.ajalt.clikt.parameters.options.versionOption
 import net.maizegenetics.phgv2.pathing.BuildKmerIndex
 import net.maizegenetics.phgv2.pathing.FindPaths
-import net.maizegenetics.phgv2.pathing.ImputationMetrics
 import net.maizegenetics.phgv2.pathing.MapKmers
 import net.maizegenetics.phgv2.utils.phgVersion
 import net.maizegenetics.phgv2.utils.setupDebugLogging
@@ -33,8 +32,7 @@ fun main(args: Array<String>) = Phg()
     .subcommands(
         SetupEnvironment(), Initdb(), CreateRanges(), PrepareAssemblies(), AgcCompress(), AlignAssemblies(), PrepareSlurmAlignFile(),
         CreateAnchorwaveDotplot(), CreateRefVcf(), CreateMafVcf(), Gvcf2Hvcf(), Hvcf2Gvcf(), LoadVcf(), ExportVcf(),
-        BuildKmerIndex(), MapKmers(), FindPaths(), ImputationMetrics(),// Imputation
-        CreateFastaFromHvcf(), MergeHvcfs(), MergeGVCFs(), CalcVcfMetrics(), StartServer, // Utilities
-        CreateHaplotypeVCF() // resequencing pipeline
+        BuildKmerIndex(), MapKmers(), FindPaths(), // Imputation
+        CreateFastaFromHvcf(), ListSamples(), MergeHvcfs(), MergeGVCFs(), CalcVcfMetrics(), StartServer // Utilities
     )
     .main(args)
