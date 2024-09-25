2de8cf2a62ef995b563f9cd6f50b6e49982e4050
lynnjo <lcj34@cornell.edu>
2024-09-25 15:15:39 -0400

    added CreateHaplotypeVCF() back to Phg.kt

--- src/main/kotlin/net/maizegenetics/phgv2/cli/Phg.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/cli/Phg.kt
@@ -33,6 +33,7 @@ fun main(args: Array<String>) = Phg()
         SetupEnvironment(), Initdb(), CreateRanges(), PrepareAssemblies(), AgcCompress(), AlignAssemblies(), PrepareSlurmAlignFile(),
         CreateAnchorwaveDotplot(), CreateRefVcf(), CreateMafVcf(), Gvcf2Hvcf(), Hvcf2Gvcf(), LoadVcf(), ExportVcf(),
         BuildKmerIndex(), MapKmers(), FindPaths(), // Imputation
-        CreateFastaFromHvcf(), ListSamples(), MergeHvcfs(), MergeGVCFs(), CalcVcfMetrics(), StartServer // Utilities
+        CreateFastaFromHvcf(), ListSamples(), MergeHvcfs(), MergeGVCFs(), CalcVcfMetrics(), StartServer, // Utilities
+        CreateHaplotypeVCF() // resequencing pipeline
     )
     .main(args)
