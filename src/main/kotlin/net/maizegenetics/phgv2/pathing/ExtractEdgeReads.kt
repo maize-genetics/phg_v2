0e7e81b624a519ec67fa0ecaa82744e8bba39972
Zack Miller <zrm22@cornell.edu>
2024-10-01 15:43:41 -0400

    Updating edge case finder

--- src/main/kotlin/net/maizegenetics/phgv2/pathing/ExtractEdgeReads.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/pathing/ExtractEdgeReads.kt
@@ -7,6 +7,7 @@ import htsjdk.samtools.SAMRecord
 import htsjdk.samtools.SamReaderFactory
 import net.maizegenetics.phgv2.api.HaplotypeGraph
 import net.maizegenetics.phgv2.api.ReferenceRange
+import net.maizegenetics.phgv2.api.SampleGamete
 import java.io.File
 
 enum class AlignmentClass {
@@ -23,6 +24,8 @@ class ExtractEdgeReads : CliktCommand( help = "Extract out Edge Case reads from 
         .required()
     val hvcfDir by option(help = "Folder name where the HVCFS are")
         .required()
+    val sampleName by option(help = "Sample name to use for the reads")
+        .required()
 
     override fun run() {
 
@@ -35,12 +38,12 @@ class ExtractEdgeReads : CliktCommand( help = "Extract out Edge Case reads from 
 
         //extract the edge reads
         for(bamFile in bamFiles) {
-            extractReads(bamFile, graph)
+            extractReads(sampleName, bamFile, graph)
         }
         TODO("Not yet implemented")
     }
 
-    fun extractReads(bamFile: String, graph: HaplotypeGraph) {
+    fun extractReads(sampleName: String, bamFile: String, graph: HaplotypeGraph) {
         //load in the reads
         val samReader = SamReaderFactory.makeDefault().open(File(bamFile))
         val iterator = samReader.iterator()
@@ -48,12 +51,13 @@ class ExtractEdgeReads : CliktCommand( help = "Extract out Edge Case reads from 
         var recordsForRead = mutableListOf<SAMRecord>()
 
         val hapIdToRefRangeMap = graph.hapIdToRefRangeMap()
+        val hapIdsToSampleGametes = graph.hapIdsToSampleGametes()
 
         while(iterator.hasNext()) {
             val currentRecord = iterator.next()
             if(currentRecord.readName != currentReadId) {
                 //process the reads
-                processReads(recordsForRead, hapIdToRefRangeMap)
+                processReads(sampleName, recordsForRead, hapIdToRefRangeMap, hapIdsToSampleGametes)
                 //reset the records
                 recordsForRead = mutableListOf()
                 currentReadId = currentRecord.readName
@@ -61,15 +65,15 @@ class ExtractEdgeReads : CliktCommand( help = "Extract out Edge Case reads from 
             recordsForRead.add(currentRecord)
         }
         //process the last read alignments
-        processReads(recordsForRead, hapIdToRefRangeMap)
+        processReads(sampleName, recordsForRead, hapIdToRefRangeMap, hapIdsToSampleGametes)
     }
 
-    fun processReads(recordsForRead: List<SAMRecord>, hapIdToRefRangeMap: Map<String, List<ReferenceRange>>) {
+    fun processReads(sampleName:String, recordsForRead: List<SAMRecord>, hapIdToRefRangeMap: Map<String, List<ReferenceRange>>, hapIdToSampleGamete: Map<String,List<SampleGamete>>) {
         //Pair off the reads by their alignment to haplotype ids
-        val recordsGroupedByContig = recordsForRead.groupBy { record -> record.contig }.map { filterAlignmentToPair(it.value) }
+        val recordsGroupedByContig = recordsForRead.groupBy { record -> hapIdToSampleGamete[record.contig]!! }.map { filterAlignmentToPair(it.value) }
         //For the pair only keep track of the best ones based on edit distance
         //We are looking for various edge cases
-        classifyAlignments(recordsGroupedByContig)
+        classifyAlignments(sampleName, recordsGroupedByContig, hapIdToRefRangeMap)
     }
 
     fun filterAlignmentToPair(records: List<SAMRecord>): Pair<SAMRecord?,SAMRecord?> {
@@ -82,14 +86,19 @@ class ExtractEdgeReads : CliktCommand( help = "Extract out Edge Case reads from 
         return records.minByOrNull { it.getIntegerAttribute("NM") }
     }
 
-    fun classifyAlignments(records: List<Pair<SAMRecord?,SAMRecord?>>): AlignmentClass {
+    fun classifyAlignments(sampleName: String, records: List<Pair<SAMRecord?,SAMRecord?>>, hapIdToRefRangeMap: Map<String, List<ReferenceRange>>): AlignmentClass {
         return when {
             records.size == 1 && records.first().first == null && records.first().second == null -> AlignmentClass.PAIRUNALIGN
             records.size == 1 && (records.first().first == null || records.first().second == null) -> AlignmentClass.UNIQUE_UNALIGN
             records.size == 1 -> AlignmentClass.PAIRUNIQUE
+            records.size in 2 .. 5 -> classifyRareAlignments(sampleName, records, hapIdToRefRangeMap )
 
             else -> AlignmentClass.PAIRUNALIGN
         }
 
+    }
+
+    fun classifyRareAlignments(sampleName: String, records: List<Pair<SAMRecord?,SAMRecord?>>, hapIdToRefRangeMap: Map<String, List<ReferenceRange>>): AlignmentClass {
+        TODO("Finish implementing this.")
     }
 }
