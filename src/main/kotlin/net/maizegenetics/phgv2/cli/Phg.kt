d2c1bded9e251c46463aae4d146abe92d97a79aa
lynnjo <lcj34@cornell.edu>
2024-10-31 12:26:30 -0400

    added PathsToGff back to Phg.kt

--- src/main/kotlin/net/maizegenetics/phgv2/cli/Phg.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/cli/Phg.kt
@@ -34,7 +34,7 @@ fun main(args: Array<String>) = Phg()
         SetupEnvironment(), Initdb(), CreateRanges(), PrepareAssemblies(), AgcCompress(), AlignAssemblies(), PrepareSlurmAlignFile(),
         CreateAnchorwaveDotplot(), CreateRefVcf(), CreateMafVcf(), Gvcf2Hvcf(), Hvcf2Gvcf(), LoadVcf(), ExportVcf(),
         BuildKmerIndex(), MapKmers(), FindPaths(), // Imputation
-        CreateFastaFromHvcf(), ListSamples(), MergeHvcfs(), MergeGVCFs(), CalcVcfMetrics(), StartServer, ExtractEdgeReads(), // Utilities
+        CreateFastaFromHvcf(), ListSamples(), MergeHvcfs(), MergeGVCFs(), CalcVcfMetrics(), StartServer, ExtractEdgeReads(), PathsToGff(), // Utilities
         CompositeToHaplotypeCoords() // resequencing pipeline
     )
     .main(args)
