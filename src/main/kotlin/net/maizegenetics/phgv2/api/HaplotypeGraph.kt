4a6bb4b456aa736ac5e1f76b0f187339f041ad71
Terry Casstevens <tmc46@cornell.edu>
2024-10-31 14:51:34 -0400

    Initial HaplotypeGraph method to get ranges from TileDB

--- src/main/kotlin/net/maizegenetics/phgv2/api/HaplotypeGraph.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/api/HaplotypeGraph.kt
@@ -1,16 +1,13 @@
 package net.maizegenetics.phgv2.api
 
-import biokotlin.util.bufferedReader
 import htsjdk.variant.variantcontext.VariantContext
 import htsjdk.variant.vcf.VCFFileReader
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import net.maizegenetics.phgv2.utils.AltHeaderMetaData
 import net.maizegenetics.phgv2.utils.parseALTHeader
-import net.maizegenetics.phgv2.utils.retrieveRefSampleName
 import org.apache.logging.log4j.LogManager
 import java.io.File
-import java.nio.file.Files
 import java.util.*
 
 /**
@@ -298,78 +295,6 @@ class HaplotypeGraph(hvcfFiles: List<String>, dbPath: String? = null) {
         refRangeMap = rangeMap.toSortedMap()
 
         rangeByGameteIdToHapid = convert(rangeIdToSampleToChecksum)
-
-    }
-
-    private fun getRanges(dbPath: String): Map<ReferenceRange, Int> {
-
-        val sampleName = retrieveRefSampleName(dbPath, "")
-
-        val tmpDir = {
-            val tmpDir = Files.createTempDirectory("haplotypeGraphTemp").toFile()
-            tmpDir.deleteOnExit()
-            tmpDir
-        }
-
-        val outputFile = "$tmpDir/all-ranges.txt"
-
-        // conda run -n phgv2-conda tiledbvcf export
-        // --uri /workdir/lcj34/phgv2_commonSept2024/tiledb_maize/hvcf_dataset
-        // -Ot --tsv-fields "CHR,POS" --sample-names B73 -o all-ranges.txt
-        val command = mutableListOf(
-            "conda",
-            "run",
-            "-n",
-            "phgv2-conda",
-            "tiledbvcf",
-            "export",
-            "--uri",
-            "$dbPath/hvcf_dataset",
-            "-Ot",
-            "--tsv-fields",
-            "CHR,POS",
-            "--sample-names",
-            sampleName,
-            "-o",
-            outputFile
-        )
-
-        val builder = ProcessBuilder(command)
-
-        val redirectError = "$tmpDir/haplotype-graph_error.log"
-        val redirectOutput = "$tmpDir/haplotype-graph_output.log"
-        builder.redirectOutput(File(redirectOutput))
-        builder.redirectError(File(redirectError))
-
-        myLogger.info("HaploytpeGraph Command: " + builder.command().joinToString(" "))
-        val process = builder.start()
-        val error = process.waitFor()
-        if (error != 0) {
-            myLogger.error("tiledbvcf export for reference: $sampleName run via ProcessBuilder returned error code $error")
-            throw IllegalStateException("tiledbvcf export for reference: $sampleName run via ProcessBuilder returned error code $error")
-        }
-
-        val rangeMap = mutableMapOf<ReferenceRange, Int>()
-
-        // SAMPLE  CHR     POS
-        // B73     chr1    1
-        // B73     chr1    34617
-        // B73     chr1    40205
-        // B73     chr1    41214
-        bufferedReader(outputFile).useLines { lines ->
-            lines
-                .drop(1)
-                .forEach { line ->
-                    val tokens = line.split("\t")
-                    val start = tokens[2].toInt()
-                    val range = ReferenceRange(tokens[1], start, start)
-                    rangeMap[range] = rangeMap.size
-                }
-        }
-
-        val rangeIdToSampleToChecksum = Array<MutableList<Array<String?>>>(rangeMap.size) { mutableListOf() }
-
-        return rangeMap
 
     }
 
