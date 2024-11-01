3c6e63ac7d8467365885316c853c55540266d9bb
lynnjo <lcj34@cornell.edu>
2024-11-01 12:02:29 -0400

    initial stub of BuildRamEfficientKmerIndex

---
+++ src/main/kotlin/net/maizegenetics/phgv2/pathing/BuildRamEfficientKmerIndex.kt
@@ -0,0 +1,63 @@
+package net.maizegenetics.phgv2.pathing
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.types.double
+import com.github.ajalt.clikt.parameters.types.int
+import com.github.ajalt.clikt.parameters.types.long
+import net.maizegenetics.phgv2.api.ReferenceRange
+import org.apache.logging.log4j.LogManager
+
+class BuildRamEfficientKmerIndex: CliktCommand(help="Create a kmer index for a HaplotypeGraph. By default the file will be written " +
+        "to <hvcfDir>/kmerIndex.txt") {
+
+    private val myLogger = LogManager.getLogger(BuildKmerIndex::class.java)
+
+    val dbPath by option(help = "Tile DB URI")
+        .required() //Needs to be required now due to the agc archive
+
+    val indexFile by option(help = "The full path of the kmer index file. Default = <hvcf-dir>/kmerIndex.txt")
+        .default("")
+
+    val discardFile by option(help = "The full path of the discard set file. If Left blank no file will be written out.")
+        .default("")
+
+    val maxHaplotypeProportion by option("-p", "--maxHapProportion", help = "only kmers mapping to less than or " +
+            "equal to maxHapProportion of haplotypes in a reference range will be retained.")
+        .double()
+        .default(0.75)
+
+    val hashMask by option("-m", "--hashMask", help = "with hashFilter, used to mask kmers for filtering. " +
+            "Default uses only the last kmer nucleotide. Only change this if you know what you are doing.")
+        .long()
+        .default(3)
+
+    val hashFilterValue by option("-f", "--hashFilter", help = "Only hashes that pass the filter" +
+            " ((hashValue and hashMask) == hashFilter) will be considered. Do not change this value unless you know " +
+            "what you are doing.")
+        .long()
+        .default(1)
+
+    val hvcfDir by option("--hvcf-dir", help = "Path to directory holding hVCF files. Data will be pulled directly from these files instead of querying TileDB")
+        .required()//Todo: make this optional by adding .default("")
+
+    val maxArgLength by option(help="The maximum argument length for a call to agc." +
+            "If you get an error caused by a call to agc being too long try reducing this value.")
+        .int()
+        .default(200000)
+
+    val noDiagnostics by option("-n", "--no-diagnostics", help = "Flag that will suppress writing of diagnostics.").flag()
+
+    val initialKeepSize by option(help="Initial size of the keep map.  Default is 500,000,000")
+        .int()
+        .default(500_000_000)
+
+    private val refrangeToAdjacentHashCount = mutableMapOf<ReferenceRange, Int>()
+    private var runDiagnostics = true
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+}
