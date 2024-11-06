adf8b1bf8c4ee55f9c0eae9f37a8d011d1400fa3
lynnjo <lcj34@cornell.edu>
2024-11-06 14:56:41 -0500

    saved Phg.kt from main

--- src/main/kotlin/net/maizegenetics/phgv2/cli/Phg.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/cli/Phg.kt
@@ -5,10 +5,7 @@ import com.github.ajalt.clikt.core.context
 import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.output.MordantHelpFormatter
 import com.github.ajalt.clikt.parameters.options.versionOption
-import net.maizegenetics.phgv2.pathing.BuildKmerIndex
-import net.maizegenetics.phgv2.pathing.ExtractEdgeReads
-import net.maizegenetics.phgv2.pathing.FindPaths
-import net.maizegenetics.phgv2.pathing.MapKmers
+import net.maizegenetics.phgv2.pathing.*
 import net.maizegenetics.phgv2.utils.phgVersion
 import net.maizegenetics.phgv2.utils.setupDebugLogging
 
@@ -34,7 +31,7 @@ fun main(args: Array<String>) = Phg()
         SetupEnvironment(), Initdb(), CreateRanges(), PrepareAssemblies(), AgcCompress(), AlignAssemblies(), PrepareSlurmAlignFile(),
         CreateAnchorwaveDotplot(), CreateRefVcf(), CreateMafVcf(), Gvcf2Hvcf(), Hvcf2Gvcf(), LoadVcf(), ExportVcf(),
         BuildKmerIndex(), MapKmers(), FindPaths(), // Imputation
-        CreateFastaFromHvcf(), ListSamples(), MergeHvcfs(), MergeGVCFs(), CalcVcfMetrics(), StartServer, ExtractEdgeReads(), PathsToGff(), // Utilities
+        CreateFastaFromHvcf(), ListSamples(), MergeHvcfs(), MergeGVCFs(), CalcVcfMetrics(), StartServer, ExtractEdgeReads(),QcReadMapping(), // Utilities
         CompositeToHaplotypeCoords() // resequencing pipeline
     )
     .main(args)
