69f03ef669f2cc4910845fb2fff66b8993c18eb7
lynnjo <lcj34@cornell.edu>
2024-11-13 08:44:54 -0500

    added HapidSampletable to Phg.kt

--- src/main/kotlin/net/maizegenetics/phgv2/cli/HapidSampleTable.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/cli/HapidSampleTable.kt
@@ -2,7 +2,6 @@ package net.maizegenetics.phgv2.cli
 
 import biokotlin.util.bufferedWriter
 import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import net.maizegenetics.phgv2.api.HaplotypeGraph
