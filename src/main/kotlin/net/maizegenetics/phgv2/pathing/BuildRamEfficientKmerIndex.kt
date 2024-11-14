538c9f6c75f5775e71f68d4e8fa3705bc017e13e
lynnjo <lcj34@cornell.edu>
2024-11-14 14:21:27 -0500

    more stuff stubbed out or copied into BUildRamEfficientKmerIndex

--- src/main/kotlin/net/maizegenetics/phgv2/pathing/BuildRamEfficientKmerIndex.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/pathing/BuildRamEfficientKmerIndex.kt
@@ -1,5 +1,6 @@
 package net.maizegenetics.phgv2.pathing
 
+import biokotlin.seq.NucSeq
 import biokotlin.util.bufferedWriter
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.default
@@ -13,7 +14,10 @@ import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap
 import it.unimi.dsi.fastutil.longs.LongOpenHashSet
 import net.maizegenetics.phgv2.api.HaplotypeGraph
 import net.maizegenetics.phgv2.api.ReferenceRange
+import net.maizegenetics.phgv2.api.SampleGamete
 import net.maizegenetics.phgv2.pathing.AlignmentUtils.Companion.buildHaplotypeGraph
+import net.maizegenetics.phgv2.utils.AltHeaderMetaData
+import net.maizegenetics.phgv2.utils.retrieveAgcContigs
 import org.apache.logging.log4j.LogManager
 import java.io.File
 import kotlin.time.DurationUnit
@@ -118,6 +122,47 @@ class BuildRamEfficientKmerIndex: CliktCommand(help="Create a kmer index for a H
         val samples = sampleGametes.map { it.name }.sorted()
 
         // TODO - implement the rest of this!
+        val contigRangesMap = graph.rangesByContig()
+
+        //We set this to be numSampleGametes * .5 so we only keep track of informative kmers
+        //We originally tried 2 * numSampleGametes but that was too high as we had too much repetitive mappings
+        val maxHapsToKeep = sampleGametes.size * .5
+
+        for (chr in contigRangesMap.keys) {
+            //get all sequence for this chromosome
+            val agcRequestLists = BuildKmerIndex.rangeListsForAgcCommand(graph, contigRangesMap, chr)
+            val agcChromSequence = if (agcRequestLists.sampleContigList.isNotEmpty()) {
+                myLogger.info("sampleContigList size = ${agcRequestLists.sampleContigList.size}, first element = ${agcRequestLists.sampleContigList[0]}")
+                retrieveAgcContigs(dbPath, agcRequestLists.sampleContigList,"")
+            }
+            else emptyMap()
+            val agcOtherRegionSequence: Map<Pair<String,String>, NucSeq> = if (agcRequestLists.otherRegionsList.isNotEmpty()) getAgcSequenceForRanges(agcRequestLists.otherRegionsList)
+            else emptyMap()
+
+            //iterate through refranges, generate kmers from the sequence
+            for (refrange in contigRangesMap[chr]!!) {
+                val hapidToSampleMap = graph.hapIdToSampleGametes(refrange)
+
+                //map haplotype ids to the sequence for that hapid
+                val hapidToSequenceMap = extractSequenceForCurrentHapIds(hapidToSampleMap, graph, agcChromSequence, agcOtherRegionSequence)
+
+                //  TODO - implement this function - it will have most of our changes based on keepMap changes
+                // We  will need to add the hapidToSampleMap as a parameter - this will be needed to get the sample
+                // for the hapid to index into the bit array
+                //Count Kmer Hashes for the Haplotype Sequences
+                //countKmerHashesForHaplotypeSequenceSimplified(hapidToSequenceMap, hashMask, hashFilterValue, keepMap, discardSet, maxHapsToKeep)
+            }
+        }
+        myLogger.debug("Finished building kmer keep set, keep set size = ${keepMap.size}, discard set size = ${discardSet.size}, elapse time ${(System.nanoTime() - startTime)/1e9} sec")
+
+        // TODO - THIS NEEDS TO CHANGE based on new keepMap definition.
+        //make sure no hashes in the keepMap are also in discard
+        var numberInDiscardSet = 0
+//        for (element in keepMap.long2ObjectEntrySet()) {
+//            if (discardSet.contains(element.longKey)) numberInDiscardSet++
+//        }
+//        myLogger.info("$numberInDiscardSet kmers in the keepMap are also in the discardSet.")
+
 
         return Pair(keepMap, discardSet)
 
@@ -134,6 +179,76 @@ class BuildRamEfficientKmerIndex: CliktCommand(help="Create a kmer index for a H
     private fun writeDiagnostics(refrangeToAdjacentHashCount: MutableMap<ReferenceRange, Int>) {
         TODO("Not yet implemented")
 
+    }
+
+    /**
+     * Function to extract out the sequence for a current list of haplotype ids
+     */
+    private fun extractSequenceForCurrentHapIds(
+        hapidToSampleMap: Map<String, List<SampleGamete>>,
+        graph: HaplotypeGraph,
+        agcChromSequence: Map<Pair<String, String>, NucSeq>,
+        agcOtherRegionSequence: Map<Pair<String, String>, NucSeq>
+    ): Map<String, List<String>> {
+
+        val hapIdToSequenceMap = hapidToSampleMap.keys.associateWith {
+            val altHeader = graph.altHeader(it)!!
+            getSeqListForHapId(altHeader, agcChromSequence, agcOtherRegionSequence, it, hapidToSampleMap)
+        }
+
+        return hapIdToSequenceMap
+    }
+
+
+    private fun getAgcSequenceForRanges(ranges: List<String>): Map<Pair<String,String>, NucSeq> {
+        val argLength = ranges.sumOf { it.length } / ranges.size + 1
+        val maxArgs = maxArgLength / argLength
+        val sequenceMap = mutableMapOf<Pair<String,String>, NucSeq>()
+        ranges.windowed(maxArgs, maxArgs, true).forEach {
+            sequenceMap.putAll(retrieveAgcContigs(dbPath, it,""))
+        }
+        return sequenceMap
+    }
+
+    /**
+     * Function to pull the needed sequences from AGC
+     */
+    private fun getSeqListForHapId(
+        altHeader: AltHeaderMetaData,
+        agcChromSequence: Map<Pair<String, String>, NucSeq>,
+        agcOtherRegionSequence: Map<Pair<String, String>, NucSeq>,
+        hapid: String,
+        hapidToSampleMap: Map<String, List<SampleGamete>>
+    ): List<String> {
+        val sequenceList = altHeader.regions.map { region ->
+            //Regions need to be corrected for any inversions. That is, when requesting sequence
+            // make sure that  range.start < range.end.
+            // Note that agc positions are 1-based but nucseq positions are 0-based.
+            // Because chromosome names can vary between assemblies, for any given assembly
+            // the chromosome name in agcChromSequence may be specific to that assembly.
+            // To deal with that, first check whether a needed sampleName, contig is in agcChromSequence.
+            // If so, use that. If not, get the sequence from afcOtherRegionSequence
+
+            //when requesting from agc, use this range
+            val seqRangeStr =
+                if (region.first.position <= region.second.position) "${region.first.position}-${region.second.position}"
+                else "${region.second.position}-${region.first.position}"
+            //when requesting from a NucSeq use this range
+            val nucseqRange =
+                if (region.first.position <= region.second.position) (region.first.position - 1..<region.second.position)
+                else (region.second.position - 1..<region.first.position)
+
+            val contigNuqseq = agcChromSequence[Pair(altHeader.sampleName(), region.first.contig)]
+
+            val regionNucSeq = if (contigNuqseq != null) contigNuqseq[nucseqRange] else {
+                agcOtherRegionSequence[Pair(altHeader.sampleName(), "${region.first.contig}:$seqRangeStr")]
+            }
+
+            //regionNucSeq should always be found. If it is not, throw an error because something has gone wrong somewhere.
+            check(regionNucSeq != null) { "No sequence for ${altHeader.sampleName()} at ${region.first.contig}:$seqRangeStr; hapid $hapid, sample ${hapidToSampleMap[hapid]}" }
+            regionNucSeq.seq()
+        }
+        return sequenceList
     }
 
 }
