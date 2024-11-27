c7d41b09a3893b192e4ece8d3c5a1798c2e7bada
lynnjo <lcj34@cornell.edu>
2024-11-27 08:18:30 -0500

    adding BUildRamEfficientKmerIndex to PHG.kt

--- src/main/kotlin/net/maizegenetics/phgv2/cli/Phg.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/cli/Phg.kt
@@ -30,7 +30,7 @@ fun main(args: Array<String>) = Phg()
     .subcommands(
         SetupEnvironment(), Initdb(), CreateRanges(), PrepareAssemblies(), AgcCompress(), AlignAssemblies(), PrepareSlurmAlignFile(),
         CreateAnchorwaveDotplot(), CreateRefVcf(), CreateMafVcf(), Gvcf2Hvcf(), Hvcf2Gvcf(), LoadVcf(), ExportVcf(),
-        BuildKmerIndex(), MapKmers(), FindPaths(), HapidSampleTable(), SampleHapidByRange(), // Imputation
+        BuildKmerIndex(), MapKmers(), FindPaths(), HapidSampleTable(), SampleHapidByRange(), BuildRamEfficientKmerIndex(),// Imputation
         CreateFastaFromHvcf(), ListSamples(), MergeHvcfs(), MergeGVCFs(), CalcVcfMetrics(), StartServer, ExtractEdgeReads(), QcReadMapping(), // Utilities
         CompositeToHaplotypeCoords() // resequencing pipeline
     )
