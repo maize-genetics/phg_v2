842ff7a538a5256e5377288f4de46ef3f111e7c5
Terry <tmc46@cornell.edu>
2024-12-09 14:47:07 -0500

    Added utility method to log Clikt Command name and parameter values when executed.
    Added utility method to log Clikt Command name and parameter values when executed.

---
+++ src/main/kotlin/net/maizegenetics/phgv2/cli/CliktLogging.kt
@@ -0,0 +1,28 @@
+package net.maizegenetics.phgv2.cli
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.OptionWithValues
+import org.apache.logging.log4j.LogManager
+
+private val myLogger = LogManager.getLogger("net.maizegenetics.phgv2.cli.CliktLogging")
+
+
+/**
+ * Log the command name and all parameters and their values.
+ */
+fun logCommand(command: CliktCommand) {
+
+    command.currentContext.originalArgv.joinToString(" ").let {
+        myLogger.info("PHGv2 Command: $it")
+    }
+
+    command.currentContext.command.registeredOptions().forEach {
+        try {
+            val option = it as OptionWithValues<*, *, *>
+            println("    ${option.names} = ${option.value}")
+        } catch (e: Exception) {
+            // do nothing
+        }
+    }
+
+}
