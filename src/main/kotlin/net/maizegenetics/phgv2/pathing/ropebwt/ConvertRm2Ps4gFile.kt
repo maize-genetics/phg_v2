57a46169e248b2dfcb624ec6001b108770f5688e
Zack Miller <zrm22@cornell.edu>
2025-01-27 16:32:33 -0500

    Needs some tests but should work correctly.

--- src/main/kotlin/net/maizegenetics/phgv2/pathing/ropebwt/ConvertRm2Ps4gFile.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/pathing/ropebwt/ConvertRm2Ps4gFile.kt
@@ -1,17 +1,21 @@
 package net.maizegenetics.phgv2.pathing.ropebwt
 
 import biokotlin.util.bufferedReader
+import biokotlin.util.bufferedWriter
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.int
 import net.maizegenetics.phgv2.api.HaplotypeGraph
+import net.maizegenetics.phgv2.api.SampleGamete
 import net.maizegenetics.phgv2.cli.headerCommand
 import net.maizegenetics.phgv2.cli.logCommand
 import net.maizegenetics.phgv2.utils.Position
 import org.apache.logging.log4j.LogManager
 
+
+data class PS4GData(val gameteList: List<Int>, val pos: Int, val count: Int)
 /**
  * Command to convert the Read Mapping file to a Position Support Gamete File
  *
@@ -43,6 +47,8 @@ class ConvertRm2Ps4gFile : CliktCommand(help = "Convert Read Mapping file to Pos
     val hvcfDir by option(help = "Directory containing the hvcf files")
         .required()
 
+    val numBitsToShift = 8 //Maybe make this a param
+
 
     override fun run() {
         logCommand(this)
@@ -55,15 +61,17 @@ class ConvertRm2Ps4gFile : CliktCommand(help = "Convert Read Mapping file to Pos
 
     fun convertReadMappingFile(readMappingFile: String, outputDir: String, binSize: Int, graph: HaplotypeGraph) {
 
-        val hapIdToPosMap = buildHapIdToPosMap(graph)
 
         val (header,readMappings) = readInReadMappingFile(readMappingFile)
 
         val cliCommand = headerCommand(this)
 
-        val ps4GData = convertReadMappingDataToPS4G(readMappings, hapIdToPosMap, binSize)
 
-//        writeOutPS4GFile(ps4GData, outputDir, cliCommand)
+        val (ps4GData, sampleGameteCount) = convertReadMappingDataToPS4G(readMappings, binSize, graph)
+
+        val outputFile = buildOutputFile(readMappingFile, outputDir)
+
+        writeOutPS4GFile(ps4GData, sampleGameteCount, outputDir, header, cliCommand)
 
     }
 
@@ -88,26 +96,70 @@ class ConvertRm2Ps4gFile : CliktCommand(help = "Convert Read Mapping file to Pos
         return Pair(headerLines, readMappingCounts)
     }
 
-    fun convertReadMappingDataToPS4G(readMappings: Map<Array<String>,Int>, hapIdToPosMap: Map<String,List<Position>>, binSize: Int) : Map<String,Map<Position,Int>> {
-        TODO()
+    fun convertReadMappingDataToPS4G(readMappings: Map<Array<String>,Int>,
+                                     binSize: Int,graph: HaplotypeGraph ) : Pair<List<PS4GData>, Map<SampleGamete,Int>> {
 
-        val ps4GData = mutableMapOf<String,MutableMap<Position,Int>>()
-        for((hapIds,count) in readMappings) {
-            for(hapId in hapIds) {
-                val posList = hapIdToPosMap[hapId]?: listOf()
-                for(pos in posList) {
-                    val binPos = Position(pos.contig, pos.position / binSize)
-                    val posEncoded = encodePosition(binPos)
-                    val posMap = ps4GData.getOrPut(hapId) { mutableMapOf() }
-                    posMap[binPos] = posMap.getOrDefault(binPos,0) + count
+        val contigToIdxMap = graph.contigs.mapIndexed { index, s -> Pair(s,index)  }.toMap()
+
+        val gameteToIdxMap = graph.sampleGametesInGraph().mapIndexed { index, s -> Pair(s,index) }.toMap()
+
+        val hapIdToRanges = graph.hapIdToRefRangeMap()
+
+        val gameteCountMap = mutableMapOf<SampleGamete,Int>()
+
+        val ps4GData = readMappings.map { (hapIdSet,count) ->
+            val mostHitRefRange = hapIdSet.flatMap { hapId -> hapIdToRanges[hapId]?: listOf() }.maxOf { it }
+            //build binned position:
+            val binPos = Position(mostHitRefRange.contig, mostHitRefRange.start / binSize)
+            val posEncoded = encodePosition(binPos, contigToIdxMap)
+
+            //convert the hapIdSet into gametes
+            val hapIdForSampleGameteMap = graph.hapIdToSampleGametes(mostHitRefRange)
+
+            val sampleGameteIdxSorted = hapIdSet.flatMap { hapId -> hapIdForSampleGameteMap[hapId]?: listOf() }
+                .mapNotNull { sampleGamete ->
+                    gameteCountMap[sampleGamete] = gameteCountMap.getOrDefault(sampleGamete,0) + count
+                    gameteToIdxMap[sampleGamete]
                 }
-            }
+                .sorted()
+
+            PS4GData(sampleGameteIdxSorted, posEncoded, count)
         }
-        return ps4GData
+
+        return Pair(ps4GData, gameteCountMap)
     }
 
-    fun encodePosition(pos: Position) : String {
+    fun encodePosition(pos: Position, contigOrdering: Map<String, Int>) : Int {
         //Pack into an Int
-        return "${pos.contig}:${pos.position}"
+        val idx = contigOrdering[pos.contig]?: throw IllegalArgumentException("Contig ${pos.contig} not found in contigOrdering")
+
+        //pack last 4 bits of idx into first 4 bits of output then pack the position minus 8 bits into the last 28 bits
+        val idxBits = idx and 0xF
+        val posBits = pos.position/256 // div 256 effectively bitshifts by 8
+
+        return (idxBits shl 28) or posBits //we dont care if its negative as we arent comparing them
+    }
+
+    fun buildOutputFile(readMappingFile: String, outputDir: String) : String {
+        val fileName = readMappingFile.split("/").last().removeSuffix(".txt")
+        return "$outputDir/${fileName}_ps4g.txt"
+    }
+
+    fun writeOutPS4GFile(pS4GData: List<PS4GData>, sampleGameteCount: Map<SampleGamete,Int>, outputFile: String, header: List<String>,cliCommand: String) {
+        bufferedWriter(outputFile).use { writer ->
+            writer.write("#PS4G\n")
+            writer.write("#ReadMappingHeader:\n")
+            header.forEach { writer.write("$it\n") }
+            writer.write("#Command: $cliCommand\n")
+            writer.write("#SampleGamete\tcount\n")
+            sampleGameteCount.forEach { (sampleGamete, count) ->
+                writer.write("#$sampleGamete\t$count\n")
+            }
+            writer.write("gameteSet\tpos\tcount\n")
+            pS4GData.forEach { (gameteList, pos, count) ->
+                writer.write("${gameteList.joinToString(",")}\t$pos\t$count\n")
+            }
+
+        }
     }
 }
