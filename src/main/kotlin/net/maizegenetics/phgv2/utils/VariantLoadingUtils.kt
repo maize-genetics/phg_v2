7ed56273c9ab437cd520f25a72b21a71e4ff2d5d
Terry <tmc46@cornell.edu>
2025-04-28 15:53:05 -0400

    Change sorting of positions. Contigs will try to be sorting numerically after removing "chr". Otherwise straight alphabetic sorting as before.

--- src/main/kotlin/net/maizegenetics/phgv2/utils/VariantLoadingUtils.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/utils/VariantLoadingUtils.kt
@@ -36,10 +36,21 @@ private val myLogger = LogManager.getLogger("net.maizegenetics.phgv2.utils.Varia
 // It needs to be Comparable to be used in a RangeSet
 data class Position (val contig: String, val position: Int) : Comparable<Position> {
     override fun compareTo(other: Position): Int {
+
         if (this.contig == other.contig) {
             return this.position.compareTo(other.position)
         }
-        return this.contig.compareTo(other.contig)
+
+        val thisContig = this.contig.replace("chr", "", ignoreCase = true).trim()
+        val otherContig = other.contig.replace("chr", "", ignoreCase = true).trim()
+
+        return try {
+            thisContig.toInt() - otherContig.toInt()
+        } catch (e: NumberFormatException) {
+            // If we can't convert contigs to an int, then compare the strings
+            contig.compareTo(other.contig)
+        }
+
     }
 
     override fun toString(): String {
