f6fb6af5135c3ae647fcaa2821456fcc0d6694d8
Zack Miller <zrm22@cornell.edu>
2025-10-16 13:01:09 -0400

    Fixing PS4G header names and removing old encoding code.

--- src/main/kotlin/net/maizegenetics/phgv2/pathing/ropebwt/PS4GUtils.kt
+++ src/main/kotlin/net/maizegenetics/phgv2/pathing/ropebwt/PS4GUtils.kt
@@ -19,6 +19,7 @@ class PS4GUtils {
         fun writeOutPS4GFile(pS4GData: List<PS4GData>, sampleGameteCount: Map<SampleGamete,Int>, gameteToIdxMap: Map<SampleGamete,Int>, outputFile: String, header: List<String>, cliCommand: String) {
             bufferedWriter(outputFile).use { writer ->
                 writer.write("#PS4G\n")
+                writer.write("#version=2.0\n")
                 header.forEach { writer.write("#$it\n") }
                 writer.write("#Command: $cliCommand\n")
                 writer.write("#TotalUniqueCounts: ${pS4GData.map { it.count }.sum()}\n")
@@ -26,62 +27,13 @@ class PS4GUtils {
                 sampleGameteCount.forEach { (sampleGamete, count) ->
                     writer.write("#$sampleGamete\t${gameteToIdxMap[sampleGamete]}\t$count\n")
                 }
-                writer.write("gameteSet\trefContig\trefPos\tcount\n")
+                writer.write("gameteSet\trefContig\trefPosBinned\tcount\n")
                 pS4GData.forEach { (gameteList, pos, count) ->
                     writer.write("${gameteList.joinToString(",")}\t${pos.contig}\t${pos.position}\t$count\n")
                 }
 
             }
         }
-//        fun encodePosition(pos: Position, contigIndexMap: Map<String, Int>) : Int {
-//            //Pack into an Int
-//            val idx = contigIndexMap[pos.contig]?: throw IllegalArgumentException("Contig ${pos.contig} not found in contigIndexMap")
-//
-//            return encodePositionFromIdxAndPos(idx, pos.position)
-//        }
-//
-//        fun encodePositionNoLookup(pos: Position): Int {
-//            //make sure that the contig is a number
-//            val idx = pos.contig.toInt()
-//            return encodePositionFromIdxAndPos(idx, pos.position)
-//        }
-//
-//        fun encodePositionFromIdxAndPos(idx: Int, pos: Int) : Int {
-//
-//            //Pack into an Int
-//            //pack last 8 bits of idx into first 8 bits of output then pack the position minus 8 bits into the last 24 bits
-//            val idxBits = idx and 0xFF //If there are more than 256 contigs this will have unexpected issues
-//            val posBits = pos/256 // div 256 effectively bitshifts by 8
-//
-//            if(posBits > 16_777_216 || posBits < 0) {
-//                throw IllegalArgumentException("Position $pos is too large to encode in an Int. Maximum position is 16,777,216 (2^24).  We only have 24 bits of room for the position.")
-//            }
-//
-//            return (idxBits shl 24) or posBits //we dont care if its negative as we arent comparing them
-//        }
-//
-//        //This is a lossy function as we /256 the position during encoding.  So it will be in a bin of 256
-//        fun decodePosition(encodedPos : Int) : Position {
-//            val idx = encodedPos.toUInt() shr 24
-//            val pos = (encodedPos.toUInt() and 0x0FFFFFFu) * 256u
-//            return Position("$idx", pos.toInt())
-//        }
-
-        /**
-         * Function to convert the count map to a PS4GData class for easy export
-         */
-//        fun convertCountMapToPS4GData(countMap: Map<Pair<Int,List<Int>>, Int>, sortPositions: Boolean = true) : List<PS4GData> {
-//            return if(sortPositions) {
-//                countMap.map { (pair, count) ->
-//                    PS4GData(pair.second.sorted(), pair.first, count)
-//                }.sortedBy { PS4GUtils.decodePosition(it.pos) } //Need to decode it because chromosome might be in an unexpected order
-//            }
-//            else {
-//                countMap.map { (pair, count) ->
-//                    PS4GData(pair.second, pair.first, count)
-//                }
-//            }
-//        }
         fun convertCountMapToPS4GData(countMap: Map<Pair<Position,List<Int>>, Int>, sortPositions: Boolean = true) : List<PS4GData> {
             return if(sortPositions) {
                 countMap.map { (pair, count) ->
